45684b4baa9844b7dc77654d7bba916f02c56a5a
Sofia Runsten <runsten.sofia@gmail.com>
2026-05-15 15:42:53 +0200

    added desktop responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import CardWithImage from "@/components/CardWithImage";
+
 const Home = () => {
   return (
     <div className="">
+      <CardWithImage headline="bli scout" logo="" title="Vågorna Väntar" text="lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum" image=""/> 
     </div>
   );
 }
--- src/components/CardWithImage/index.tsx
+++ src/components/CardWithImage/index.tsx
@@ -11,19 +11,19 @@ interface CardWithImageProps {
 const CardWithImage = ({ headline, logo, title, text, image }: CardWithImageProps) => {
   return (
     <>
-      <div className="pt-5 px-4 pb-10">
-        <div className="pt-5 gap-2 w-82.5 h-42.75">
+      <div className="flex flex-col md:flex-row w-full pt-5 px-4 pb-10 md:flex md:pt-12.5 md:pb-25 md:px-25 md:items-start gap-6 md:gap-25 items-center">
+        <div className="pt-5 md:pt-0 gap-2 w-82.5 h-42.75 md:w-170 flex-1">
           <div className="flex gap-2 items-center ">
-            <p className="h-4.25 uppercase font-bold text-acc tracking-[1.5px] text-(--text-blue)">{headline}</p>
+            <p className="h-4.25 uppercase font-bold text-acc tracking-[1.5px] text-(--text-blue) ">{headline}</p>
             <p className="w-5 h-4.25">{logo}</p>
           </div>
           <div className="tracking-[-0.5%] text-h1 font-normal text-primary">{title}</div>
           <div className="text-[16px] tracking-[3%] font-normal text-(--text-black)">{text}</div>
         </div>
-        <Image src={image} alt={title} width={357} height={257} className="bg-amber-200 rounded-2xl" />
+        <Image src={image} alt={title} width={357} height={257} className="bg-amber-200 pt-4 rounded-2xl md:w-100 md:h-60" />
       </div>
     </>
   )
 }
 
-export default CardWithImage
+export default CardWithImage 
