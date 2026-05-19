d1db983b006fadd86c72d93f1e85474f1b1ef668
Dobeen Kim <dobin7955@gmail.com>
2026-05-19 23:09:54 +0200

    dd hover effect and transition to CTA button

--- src/components/CTA-button.tsx
+++ src/components/CTA-button.tsx
@@ -8,7 +8,10 @@ const CTABtn = ({text,onClick}:CTAprops) => {
     return (
         <div className="w-[222]">
             <button className="text-body-desktop bg-accent text-primary overflow-hidden rounded-[24]
-            p-2.5 text-center cursor-pointer" onClick={onClick}>{text}</button>
+                p-2.5 text-center cursor-pointer transition-colors duration-300 ease-in-out hover:bg-primary hover:text-accent" 
+                onClick={onClick}>
+                {text}
+            </button>
         </div>
     )
 }
