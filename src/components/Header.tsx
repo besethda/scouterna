f9eb493477f8616e4c760275dc81ae27c3dae7a1
Sofia Runsten <runsten.sofia@gmail.com>
2026-05-19 14:02:07 +0200

    color-fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,7 +12,7 @@ const Header = () => {
   const messages = useMessages()
 
   return (
-    <header className="flex justify-between items-center py-2.75 px-7 bg-(--bg-white)">
+    <header className="flex justify-between items-center py-2.75 px-7 bg-white">
       <Link href="/" className="md:hidden">
         <Image src={HSSlogo} alt='HSS-LOGO' className="w-11 h-10.5" />
       </Link>
@@ -25,7 +25,7 @@ const Header = () => {
           </div>
         </div>
       </Link>
-      <div className='hidden md:flex gap-6 font-semibold text-lg  tracking-[0.03em]'>
+      <div className='hidden md:flex gap-6 font-semibold text-link-desktop tracking-[0.03em]'>
         <Link href="" className="flex gap-2.5 items-center">
           <Image src={Hand} alt='HandLogo' className='h-8.25 w-auto' />
           <p className="font-albert tracking-[3%]">{messages?.header?.header_join}</p>
@@ -35,7 +35,7 @@ const Header = () => {
           <p className="font-albert tracking-[3%]">ScoutNet</p>
         </Link>
         <Link href="" className="flex gap-2.5 items-center">
-  
+          {/* SV/ENG HERE */}SV
         </Link>
       </div>
       <RxHamburgerMenu className="size-5.75 md:hidden" />
