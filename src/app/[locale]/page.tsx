fdffd4908d85fbeee778469afd20a9a49edeca32
Dobeen Kim <dobin7955@gmail.com>
2026-05-19 23:47:44 +0200

    add fallback background image

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-0
+
 import useMessages from "@/hook/useMessages";
 import HeroSection from "@/components/Hero-section";
 import bgImage01 from "../../../public/images/hero-img01.png"
--- src/components/Hero-section.tsx
+++ src/components/Hero-section.tsx
@@ -1,20 +1,22 @@
 import useMessages from '@/hook/useMessages'
+import undefinedBg from "../../public/images/DSCF3020.jpg"
 
 interface HeroSectionProps{
-    bgImage:{src: string};
-    title01?: string;
-    title02?:string;
-    description?:string
+    bgImage? :{src: string};
+    title01? : string;
+    title02? :string;
+    description? :string
 
 }
 
 const HeroSection = ({bgImage, title01,title02, description}:HeroSectionProps) => {
 
     const messages = useMessages()
+    const finalBg = bgImage ? bgImage.src : undefinedBg.src
 
     return (
         <div className="h-[700] w-full bg-cover bg-center bg-no-repeat flex flex-col gap-10"
-            style={{backgroundImage: bgImage?`url(${bgImage.src})`:'none'}}>
+            style={{backgroundImage: `url(${finalBg})`}}>
                 <div className="flex flex-col gap-10 xl:text-left xl:w-[527] xl:ml-[150] xl:gap-7">
                     <div className="flex gap-2 w-[350] h-[36] bg-primary mx-auto rounded-3xl 
                         border border-solid border-accent mt-16 xl:opacity-60 xl:ml-0">
