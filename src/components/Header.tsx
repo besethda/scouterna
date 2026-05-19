70d619662b2722aeecf794866e98837559bcc06a
Sofia Runsten <runsten.sofia@gmail.com>
2026-05-19 13:17:19 +0200

    fixes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -21,19 +21,19 @@ const Header = () => {
         <div className="flex gap-2 items-center">
           <Image src={HSSlogo} alt='HSS-LOGO' className=" w-25 h-23.75" />
           <div className=" p-2.5 items-center text-xl/6 ">
-            <div className="font-normal ">{messages?.header?.header_title}</div>
-            <div className="font-bold ">{messages?.header?.header_subtitle}</div>
+            <div className="font-normal font-fraunces">{messages?.header?.header_title}</div>
+            <div className="font-bold font-fraunces">{messages?.header?.header_subtitle}</div>
           </div>
         </div>
       </Link>
       <div className='hidden md:flex gap-6 font-semibold text-lg  tracking-[0.03em]'>
         <Link href="" className="flex gap-2.5 items-center">
           <Image src={Hand} alt='HandLogo' className='h-8.25 w-auto' />
-          <p>{messages?.header?.header_join}</p>
+          <p className="font-albert tracking-[3%]">{messages?.header?.header_join}</p>
         </Link>
         <Link href="" className="flex gap-2.5 items-center">
           <Image src={Key} alt='key' className='h-5.75 w-auto' />
-          <p>ScoutNet</p>
+          <p className="font-albert tracking-[3%]">ScoutNet</p>
         </Link>
         <Link href="" className="flex gap-2.5 items-center">
         {/* SV/ENG here */}
