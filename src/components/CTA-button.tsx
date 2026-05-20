44352caf6b683af5cc2d83cdf1ecc6279f5f8ed2
Dobeen Kim <dobin7955@gmail.com>
2026-05-20 14:55:15 +0200

    save

--- src/components/CTA-button.tsx
+++ src/components/CTA-button.tsx
@@ -6,7 +6,7 @@ interface CTAprops {
 
 const CTABtn = ({text,onClick}:CTAprops) => {
     return (
-        <div className="w-[222]">
+        <div className="w-[222] ">
             <button className="text-body-desktop bg-accent text-primary overflow-hidden rounded-[24]
                 p-2.5 text-center cursor-pointer transition-colors duration-300 ease-in-out hover:bg-lightYellow" 
                 onClick={onClick}>
