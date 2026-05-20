492fd08b0bd36eb4b876f4bee404413f01f738c1
Dobeen Kim <dobin7955@gmail.com>
2026-05-20 15:19:18 +0200

    add size

--- src/components/CTA-button.tsx
+++ src/components/CTA-button.tsx
@@ -6,8 +6,8 @@ interface CTAprops {
 
 const CTABtn = ({text,onClick}:CTAprops) => {
     return (
-        <div className="w-[222]">
-            <button className="text-body-desktop bg-accent text-primary overflow-hidden rounded-3xl
+        <div className="w:232px">
+            <button className="!w-[232px] text-body-desktop bg-accent text-primary overflow-hidden rounded-3xl
                 p-2.5 text-center cursor-pointer transition-colors duration-300 ease-in-out hover:bg-lightYellow" 
                 onClick={onClick}>
                 {text}
