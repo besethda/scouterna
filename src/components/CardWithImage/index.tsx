b41c8af7ceac9437494b11c517e304dcc271bd3f
Sofia Runsten <runsten.sofia@gmail.com>
2026-05-20 16:02:23 +0200

    fixes fonts and icon

--- src/components/CardWithImage/index.tsx
+++ src/components/CardWithImage/index.tsx
@@ -14,12 +14,17 @@ const CardWithImage = ({ headline, logo, title, text, image }: CardWithImageProp
       <div className="flex flex-col md:flex-row w-full pt-5 px-4 pb-10 md:flex md:pt-12.5 md:pb-25 md:px-25 md:items-start gap-6 md:gap-25 items-center">
         <div className="pt-5 md:pt-0 gap-2 w-82.5 h-42.75 md:w-170 flex-1">
           <div className="flex gap-2 items-center ">
-            <p className="h-4.25 uppercase font-bold text-acc tracking-[1.5px] text-(--text-blue) ">{headline}</p>
-            <div className=" flex items-center justify-center md:bg-amber-500 md:h-11 md:w-11 rounded-[50%] ">
-            <Image src={logo} alt="logo" width={20} height={17} className="w-5 h-4.25"/>
+            <p className="h-4.25 uppercase font-bold text-acc tracking-[1.5px] text-(--text-blue) md:text-[14px] ">{headline}</p>
+            <div className=" flex items-center justify-center rounded-[50%] md:hidden">
+              <Image src={logo} alt="logo" width={20} height={17} className="w-5 h-4.25" />
             </div>
           </div>
-          <div className="tracking-[-0.5%] text-h1 font-normal text-primary">{title}</div>
+          <div className="md:flex md:flex-row md:items-center md:gap-2.5">
+            <div className="font-varela tracking-[-0.5%] text-h1 font-normal text-primary md:text-h1-desktop">{title}</div>
+            <div className="hidden md:flex items-center justify-center md:bg-accent md:h-11 md:w-11 rounded-[50%] ">
+              <Image src={logo} alt="logo" width={20} height={17} className="w-5 h-4.25" />
+            </div>
+          </div>
           <div className="text-[16px] tracking-[3%] font-normal text-(--text-black)">{text}</div>
         </div>
         <div>
