6b2eae4d96022304623b605df922513a313b6b58
Sofia Runsten <runsten.sofia@gmail.com>
2026-05-20 21:30:16 +0200

    small fix with font

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -20,7 +20,7 @@ const Header = () => {
       <Link href="/" className="hidden md:flex items-center">
         <div className="flex gap-2 items-center">
           <Image src={HSSlogo} alt='HSS-LOGO' className=" w-25 h-23.75" />
-          <div className=" p-2.5 items-center text-xl/6 ">
+          <div className=" p-2.5 items-center text-xl/6 tracking-[1px]">
             <div className="font-normal font-fraunces">{messages?.header?.header_title}</div>
             <div className="font-bold font-fraunces">{messages?.header?.header_subtitle}</div>
           </div>
@@ -29,11 +29,11 @@ const Header = () => {
       <div className='hidden md:flex gap-6 font-semibold text-link-desktop tracking-[0.03em]'>
         <Link href="" className="flex gap-2.5 items-center">
           <Image src={Hand} alt='HandLogo' className='h-8.25 w-auto' />
-          <p className="font-albert tracking-[3%]">{messages?.header?.header_join}</p>
+          <p className="font-albert  tracking-[3%] leading-[100%] text-text-black ">{messages?.header?.header_join}</p>
         </Link>
         <Link href="" className="flex gap-2.5 items-center">
           <Image src={Key} alt='key' className='h-5.75 w-auto' />
-          <p className="font-albert tracking-[3%]">ScoutNet</p>
+          <p className="font-albert tracking-[3%] leading-[100%] text-text-black  ">ScoutNet</p>
         </Link>
         <LanguageSelector />
       </div>
