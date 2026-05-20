94794b4ba372e9764c79d74e16034bf45a5adb87
Dobeen Kim <dobin7955@gmail.com>
2026-05-20 12:37:22 +0200

    changed hover color to lighty yellow and add color-lightYellow:#EBC86C; into global.css

--- src/components/CTA-button.tsx
+++ src/components/CTA-button.tsx
@@ -8,7 +8,7 @@ const CTABtn = ({text,onClick}:CTAprops) => {
     return (
         <div className="w-[222]">
             <button className="text-body-desktop bg-accent text-primary overflow-hidden rounded-[24]
-                p-2.5 text-center cursor-pointer transition-colors duration-300 ease-in-out hover:bg-primary hover:text-accent" 
+                p-2.5 text-center cursor-pointer transition-colors duration-300 ease-in-out hover:bg-lightYellow" 
                 onClick={onClick}>
                 {text}
             </button>
