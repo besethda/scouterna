804ab865f8a55305fdf47421a6e8edeed10a895f
Maho Mikami <maho.cx1029@gmail.com>
2026-05-21 09:03:22 +0200

    fix the position

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -16,7 +16,7 @@ const Navigation = () => {
     }
 
     return (
-        <div className="absolute lg:static w-full bg-white rounded-b-lg font-albert tracking-[0.03em] top-16 lg:bg-primary lg:h-25.5 lg:text-white lg:text-lg lg:rounded-none">
+        <div className="absolute lg:static w-full bg-white rounded-b-lg font-albert tracking-[0.03em] top-16 md:top-29 lg:bg-primary lg:h-25.5 lg:text-white lg:text-lg lg:rounded-none">
             <div className="w-full lg:h-full lg:flex lg:justify-around lg:items-center lg:max-w-3xl lg:mx-auto xl:max-w-5xl 2xl:max-w-7xl">
                 <div className="bg-[#D6EAF8] flex justify-end text-base h-69px px-2 py-4 font-bold border-b border-lightGray lg:hidden">
                     <LanguageSelector />
