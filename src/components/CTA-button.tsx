dea371164189547c9295c7b5770a4d9bf9c8bf7d
Seth Armistead <sdarmistead@gmail.com>
2026-05-21 17:00:16 +0200

    added hover and active effects and shadow

--- src/components/CTA-button.tsx
+++ src/components/CTA-button.tsx
@@ -8,7 +8,7 @@ const CTABtn = ({text, onClick, width = "fit"}:CTAprops) => {
     return (
         <div className="">
             <button className={`py-[18.6] px-[24] ${width === "fit" ? "w-fit" : width === "full" ? "w-full" : "w-full md:w-fit"} font-albert text-body md:text-body-desktop bg-accent text-primary overflow-hidden rounded-4xl
-                px-6 py-4 text-center cursor-pointer transition-colors duration-300 ease-in-out hover:bg-lightYellow`} 
+                px-6 py-4 text-center cursor-pointer transition-colors duration-300 shadow-md-black ease-in-out hover:shadow-none active:scale-98 active:brightness-90 hover:bg-lightYellow`} 
                 onClick={onClick}>
                 {text}
             </button>
