d32e476eef2c7c1dff098f8869fccd8e253285fb
Seth Armistead <sdarmistead@gmail.com>
2026-05-21 23:16:08 +0200

    fixed image and icon shapeshift

--- src/components/CardWithImage.tsx
+++ src/components/CardWithImage.tsx
@@ -11,7 +11,7 @@ interface CardWithImageProps {
 const CardWithImage = ({ headline, logo, title, text, image }: CardWithImageProps) => {
   return (
     <>
-      <div className="flex flex-col md:flex-row w-full pt-5 px-4 pb-10 md:flex md:pt-12.5 md:pb-0 md:px-22 md:items-start gap-6 md:gap-25 items-center lg:max-w-430">
+      <div className="flex flex-col md:flex-row w-full pt-5 px-4 pb-10 md:flex md:pt-12.5 lg:pb-12 md:px-30 md:pb-20 md:items-start gap-6 md:gap-25 items-center lg:max-w-430">
         <div className="pt-5 md:pt-0 gap-2 w-82.5 h-42.75 md:w-170 flex-1 ">
           <div className="flex gap-2 items-center ">
             <p className="h-4.25 uppercase font-bold text-acc tracking-[1.5px]  text-text-blue md:text-[14px] ">{headline}</p>
@@ -21,7 +21,7 @@ const CardWithImage = ({ headline, logo, title, text, image }: CardWithImageProp
           </div>
           <div className="md:flex md:flex-row md:items-center md:gap-2.5">
             <div className="font-varela tracking-[-0.5%] text-h1 font-normal text-primary md:text-h1-desktop">{title}</div>
-            <div className="hidden md:flex items-center justify-center md:bg-accent md:h-11 md:w-11 rounded-[50%] ">
+            <div className="hidden md:flex items-center justify-center md:bg-accent shrink-0 md:h-11 md:w-11 rounded-[50%] ">
               <Image src={logo} alt="logo" width={24} height={17} className="w-6 h-4.25" />
             </div>
           </div>
