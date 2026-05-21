e1333d26777adebdfb0a99ac8c9074dd4a2d49e8
Seth Armistead <sdarmistead@gmail.com>
2026-05-21 15:11:29 +0200

    added prop for button

--- src/components/CTA-button.tsx
+++ src/components/CTA-button.tsx
@@ -1,14 +1,14 @@
 interface CTAprops {
-    text?:string,
+    text?: string,
     onClick?:() => void,
-
+    width?: boolean
 } 
 //!w-[232px]
-const CTABtn = ({text,onClick}:CTAprops) => {
+const CTABtn = ({text, onClick, width = true}:CTAprops) => {
     return (
         <div className="">
-            <button className="py-[18.6] px-[24] text-body-desktop bg-accent text-primary overflow-hidden rounded-4xl
-                p-2.5 text-center cursor-pointer transition-colors duration-300 ease-in-out hover:bg-lightYellow" 
+            <button className={`py-[18.6] px-[24] ${width ? "w-fit" : "w-full"} text-body-desktop bg-accent text-primary overflow-hidden rounded-4xl
+                p-2.5 text-center cursor-pointer transition-colors duration-300 ease-in-out hover:bg-lightYellow`} 
                 onClick={onClick}>
                 {text}
             </button>
