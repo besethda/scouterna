9d44c264b616f296936f614bce9cc16bf38a238f
Sofia Runsten <runsten.sofia@gmail.com>
2026-05-21 20:48:50 +0200

    biggest fix of all time

--- src/components/CardWithImage.tsx
+++ src/components/CardWithImage.tsx
@@ -14,7 +14,7 @@ const CardWithImage = ({ headline, logo, title, text, image }: CardWithImageProp
       <div className="flex flex-col md:flex-row w-full pt-5 px-4 pb-10 md:flex md:pt-12.5 md:pb-25 md:px-25 md:items-start gap-6 md:gap-25 items-center">
         <div className="pt-5 md:pt-0 gap-2 w-82.5 h-42.75 md:w-170 flex-1 ">
           <div className="flex gap-2 items-center ">
-            <p className="h-4.25 uppercase font-bold text-acc tracking-[1.5px]  text-(--color-text-blue) md:text-[14px] ">{headline}</p>
+            <p className="h-4.25 uppercase font-bold text-acc tracking-[1.5px]  text-text-blue md:text-[14px] ">{headline}</p>
             <div className=" flex items-center justify-center rounded-[50%] md:hidden">
               <Image src={logo} alt="logo" width={24} height={17} className="w-6 h-4.25" />
             </div>
