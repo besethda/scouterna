3e0baeb35e27c89c49ddf0a80cf7604d44ecfcf9
Sofia Runsten <runsten.sofia@gmail.com>
2026-05-25 11:50:03 +0200

    added card with image to joinpage

--- src/app/[locale]/join/become-a-scout/page.tsx
+++ src/app/[locale]/join/become-a-scout/page.tsx
@@ -1,7 +1,18 @@
+'use client'
+import CardWithImage from "@/components/CardWithImage";
+import useMessages from "@/hook/useMessages"
+
 const BecomeAScout = () => {
+
+    const messages = useMessages()
+
+    if (!messages) {
+        return null
+    }
 
     return (
         <div>
+            <CardWithImage headline={messages?.joinCard?.headline} logo="/heart.png" title={messages?.joinCard?.title} text={messages?.joinCard?.text} image="/images/DSCF3017.jpg" />
         </div>
     );
 }
