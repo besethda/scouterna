aacbc769743a41300d319a6e4c9cb5ee89c415e6
Dobeen Kim <dobin7955@gmail.com>
2026-05-26 09:03:31 +0200

    save

--- src/messages/sv.ts
+++ src/messages/sv.ts
@@ -421,7 +421,7 @@ export const Sv: MessagesSvType = {
     },
     { 
       question: "Jag har tappat bort min faktura. Vad gör jag?", 
-      answer: `Skicka ett mejl till support@scouterna.se med ditt namn och medlemsnummer så får du en kopia av fakturan via e-post.` 
+      answer: `Skicka ett mejl till support@scouterna.se med ditt namn och medlemsnummer så får du en kopia av fakturan via e-post.` 
     },
     { 
       question: "Vad ingår i min medlemsavgift?", 
@@ -433,11 +433,11 @@ export const Sv: MessagesSvType = {
     },
     { 
       question: "Min faktura har felaktigt belopp. Vad gör jag?", 
-      answer: `Skicka ett mejl till support@scouterna.se och ange varför du anser att det är fel belopp, ditt medlemsnummer, namnet på din kår samt vilken faktura det gäller.` 
+      answer: `Skicka ett mejl till support@scouterna.se och ange varför du anser att det är fel belopp, ditt medlemsnummer, namnet på din kår samt vilken faktura det gäller.` 
     },
     { 
       question: "Hur ändrar jag mina kontaktuppgifter?", 
-      answer: `Logga in på www.scoutnet.se där kan du uppdatera dina uppgifter.` 
+      answer: `Logga in på www.scoutnet.se där kan du uppdatera dina uppgifter.` 
     },
   ]
 };
