515a04849a805e7667f65ff2afdd061b4e68bd25
Seth Armistead <sdarmistead@gmail.com>
2026-05-26 15:54:49 +0200

    fixed styling for cardwithimage

--- src/app/[locale]/members/safety/page.tsx
+++ src/app/[locale]/members/safety/page.tsx
@@ -8,13 +8,15 @@ const Safety = () => {
     const messages = useMessages()
     return (
         <>
-            <div>
-                <CardWithImage sectionTitle="safetyCard" logo="/blue-lifejacket.svg" image="/images/DSCF3070.jpg" />
-                <div className="hidden md:block pl-4 md:pl-50 pb-13">
-                    <CTABtn text={messages?.safetyBtn?.btnDesktop} />
-                </div>
-                <div className="pl-4 pb-13 md:hidden">
-                    <CTABtn text={messages?.safetyBtn?.btnMobile} />
+            <div className="flex flex-col">
+                <div className="h-fit pb-13">
+                    <CardWithImage sectionTitle="safetyCard" logo="/blue-lifejacket.svg" image="/images/DSCF3070.jpg" />
+                    <div className="hidden lg:block px-4 lg:pt-4 pt-0 md:w-full md:flex md:justify-center ">
+                        <CTABtn text={messages?.safetyBtn?.btnDesktop} />
+                    </div>
+                    <div className="pl-4 pb-13 md:hidden">
+                        <CTABtn text={messages?.safetyBtn?.btnMobile} />
+                    </div>
                 </div>
                 <div className="flex flex-col">
                     <div className="order-1 md:order-2">
--- src/components/CardWithImage.tsx
+++ src/components/CardWithImage.tsx
@@ -16,8 +16,8 @@ const CardWithImage = ({ sectionTitle, logo, image }: CardWithImageProps) => {
 
   return (
     <>
-      <div className="flex flex-col md:flex-row w-full pt-5 px-4 pb-10 md:flex md:pt-12.5 lg:pb-12 md:px-50 md:pb-20 md:items-start gap-6 md:gap-25 items-center lg:max-w-430">
-        <div className="pt-5 md:pt-0 gap-2 w-82.5 h-42.75 md:w-170 flex-1 ">
+      <div className="flex flex-col lg:flex-row w-full lg:min-w-180 pt-5 px-4 pb-10 md:flex md:pt-12.5 md:pb-8 lg:pb-15 lg:items-start gap-6 md:gap-25 items-center lg:max-w-430">
+        <div className="pt-5 md:pt-0 gap-2 h-fit flex-1 ">
           <div className="flex gap-2 items-center ">
             <p className="h-4.25 uppercase font-bold text-acc tracking-[1.5px] text-text-blue md:text-[14px] ">{messages?.[sectionTitle as string]?.headline}</p>
             <div className=" flex items-center justify-center rounded-[50%] md:hidden">
@@ -30,7 +30,7 @@ const CardWithImage = ({ sectionTitle, logo, image }: CardWithImageProps) => {
               <Image src={logo} alt="logo" width={24} height={17} className="w-auto h-4.5" />
             </div>
           </div>
-          <div className="text-[16px] text-text-black tracking-[3%] font-albert font-normal md:text-[18px]">{messages?.[sectionTitle as string]?.text}</div>
+          <div className="text-[16px] text-text-black tracking-[3%] h-fit font-albert font-normal md:text-[18px]">{messages?.[sectionTitle as string]?.text}</div>
         </div>
         <div className="relative w-full aspect-video max-w-100 lg: max-h-60 min-h-40 md:mb-34 lg:mb-0">
           <Image src={image} alt={messages?.[sectionTitle as string]?.title} fill className="rounded-2xl object-cover" />
