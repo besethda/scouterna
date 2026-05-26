98fc3a1f9233f3bcff9a73ec8a6d157b5ea99569
Dobeen Kim <dobin7955@gmail.com>
2026-05-26 15:16:25 +0200

    removed testing code

--- src/components/JoinPage.tsx
+++ src/components/JoinPage.tsx
@@ -12,7 +12,6 @@ interface JoinPageProps {
 
 const JoinPage = ({title, text, buttonText,buttonOnClick,btnWidth,width = "full"}:JoinPageProps) => {
     return (
-        /*<div className={width === "full" ? "w-full" : "w-[358] h-[254] rounded-2xl  bg-bg-blue p-8 flex flex-col gap-6"}>*/
         <div className={`${width === "box" ? "w-[358px] h-[254px] rounded-2xl md:flex-row md:w-full md:rounded items-center" : "w-full"}  p-8 flex flex-col gap-6 bg-bg-blue mx-auto md:p-17`}>
             <div className="flex flex-col gap-2"> 
                 <h2 className="text-h2 md:text-h2-desktop text-primary">{title}</h2>
