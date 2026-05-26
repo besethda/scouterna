50ab71e74fac8b2cc4d934c04df94f50c4d94be7
Dobeen Kim <dobin7955@gmail.com>
2026-05-26 08:21:14 +0200

    add translate, answers and add code in faq/ page.tsx

--- src/app/[locale]/join/faq/page.tsx
+++ src/app/[locale]/join/faq/page.tsx
@@ -1,8 +1,44 @@
+'use client'
+import useMessages from "@/hook/useMessages";
+import FragorSvar from "@/components/Fragor-svar";
+
 const Faq = () => {
+
+    const messages = useMessages()
+
+    if (!messages) {
+    return null
+    }
+
 
     return (
         <div>
+            <div className="bg-bg-blue py-10">
+            <h2 className="pb-2 text-h2 text-center md:text-h1-desktop"> {messages?.faq.title} </h2>
+            <div className="flex flex-col gap-2.5">
+                {(messages as any)?.fragorSvar?.map((item:any,index:number ) =>(
+                    <FragorSvar 
+                    key={index}
+                    question={item.question}
+                    answer={item.answer} 
+                    />
+                ))}
+            </div>
         </div>
+        <div className="py-10">
+            <h2 className="pb-2 text-h2 text-center  md:text-h1-desktop"> {messages?.faq.title02} </h2>
+            <div className="flex flex-col gap-2.5">
+                {(messages as any)?.fragorSvar02?.map((item:any,index:number ) =>(
+                    <FragorSvar 
+                    key={index}
+                    question={item.question}
+                    answer={item.answer} 
+                    />
+                ))}
+            </div>
+        </div>
+        </div>
+        
     );
 }
 
