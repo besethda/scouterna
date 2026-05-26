2dbfdd895aadc9ed026979a23f3e777d3d12b23a
Dobeen Kim <dobin7955@gmail.com>
2026-05-26 15:31:54 +0200

    save

--- src/components/JoinPage.tsx
+++ src/components/JoinPage.tsx
@@ -12,7 +12,7 @@ interface JoinPageProps {
 
 const JoinPage = ({title, text, buttonText,buttonOnClick,btnWidth,width = "full"}:JoinPageProps) => {
     return (
-        <div className={`${width === "box" ? "w-[358px] h-[254px] rounded-2xl md:flex-row md:w-full md:rounded items-center" : "w-full"}  p-8 flex flex-col gap-6 bg-bg-blue mx-auto md:p-17`}>
+        <div className={`${width === "box" ? "w-[358px] h-[254px] rounded-2xl md:flex-row md:w-full md:rounded items-center" : "w-full"} p-8 flex flex-col gap-6 bg-bg-blue mx-auto md:p-17`}>
             <div className="flex flex-col gap-2"> 
                 <h2 className="text-h2 md:text-h2-desktop text-primary">{title}</h2>
                 <p className={`text-black text-body md:text-body-desktop ${width === "box" ?" md:w-[53%]" : "w-full"}`}>{text}</p>
