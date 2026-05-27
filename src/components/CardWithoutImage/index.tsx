741435b9496d6a4faa77d4bb14a4861d14d458ae
Sofia Runsten <runsten.sofia@gmail.com>
2026-05-27 14:30:06 +0200

    fix responsiveness

--- src/components/CardWithoutImage/index.tsx
+++ src/components/CardWithoutImage/index.tsx
@@ -12,7 +12,7 @@ const CardWithoutImage = ({ headline, logo, title, text, MDlogo }: CardWithoutIm
   return (
     <>
       <div className="flex flex-col md:flex-row w-full pt-5 px-4 pb-10 md:flex md:pt-12.5 md:pb-25 md:px-50 md:items-start gap-6 md:gap-25 items-center ">
-        <div className="pt-5 md:pt-0 gap-2 w-82.5 h-42.75 md:w-170 flex-1">
+        <div className="pt-5 md:pt-0 gap-2 w-82.5 md:w-full flex-1">
           <div className="flex gap-2 items-center ">
             <p className=" h-4.25 uppercase font-bold md:font-normal text-acc md:text-[14px] tracking-[1.5px] text-text-blue font-public ">
               {headline}
