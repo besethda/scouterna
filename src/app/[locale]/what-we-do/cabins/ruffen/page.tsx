4204c4a082ba7fde27881b33a97b831889fe3c13
Maho Mikami <maho.cx1029@gmail.com>
2026-05-27 20:17:51 +0200

    add group division box to ruffen and group structure page

--- src/app/[locale]/what-we-do/cabins/ruffen/page.tsx
+++ src/app/[locale]/what-we-do/cabins/ruffen/page.tsx
@@ -1,7 +1,8 @@
 import { getPageHeadTitle } from "@/lib/utils"
 import CardWithImage from "@/components/CardWithImage";
+import DivisionsBox from "@/components/DivisionsContainer/DivisionsBox";
 import MapWrapper from "@/components/Map/MapWrapper";
-import { En} from "@/messages/en";
+import { En } from "@/messages/en";
 import { Sv } from "@/messages/sv";
 import { use } from "react";
 
@@ -12,14 +13,21 @@ export async function generateMetadata({ params }: { params: Promise<{ locale: s
     return getPageHeadTitle(locale, pageItem)
 }
 
-const Ruffen = ({params}:{params: Promise<{locale: string}>}) => {
+const Ruffen = ({ params }: { params: Promise<{ locale: string }> }) => {
 
-    const {locale} = use(params)
+    const { locale } = use(params)
     const messages = locale === "en" ? En : Sv
-   
+
     return (
         <div>
             <CardWithImage sectionTitle="ruffen" logo="/blueHouse.png" image="/images/IMG_3957.JPEG" />
+            <div className="flex flex-col p-4 gap-1 md:items-center">
+                <DivisionsBox division={"sjohumlorna"} boxStyle={"info"} />
+                <DivisionsBox division={"kaparna"} boxStyle={"info"} />
+                <DivisionsBox division={"utmanare"} boxStyle={"info"} />
+                <DivisionsBox division={"konvojen"} boxStyle={"info"} />
+                <DivisionsBox division={"familjescouter"} boxStyle={"info"} />
+            </div>
             <MapWrapper />
         </div>
     );
