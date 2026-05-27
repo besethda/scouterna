4845326a4a2f1d2c57cac9c48285cd973c4925c2
Dobeen Kim <dobin7955@gmail.com>
2026-05-27 17:54:26 +0200

    fixed font

--- src/components/JoinPage.tsx
+++ src/components/JoinPage.tsx
@@ -15,7 +15,7 @@ const JoinPage = ({title, text, buttonText,buttonOnClick,btnWidth,width = "full"
         <div className={`${width === "box" ? "w-89.5 h-63.5 md:h-83 rounded-2xl md:flex-row md:w-full md:rounded items-center" : "w-full"} p-8 flex flex-col gap-6 bg-bg-blue mx-auto md:p-25`}>
             <div className="flex flex-col gap-2 md:w-[30vw]"> 
                 <h2 className="text-h2-albert md:text-h2-desktop font-albert text-primary md:w-[40vw]">{title}</h2>
-                <p className={"text-text-black-black text-body md:text-body-desktop"}>{text}</p>
+                <p className={"text-text-black text-body md:text-body-desktop font-albert"}>{text}</p>
             </div>
             {buttonText &&(
                 <div className="mx-auto w-full md:w-fit">
