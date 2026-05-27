629060acca284fbaf9b5551d0eac928399404c5a
Seth Armistead <sdarmistead@gmail.com>
2026-05-27 16:02:27 +0200

    added breaking point for them

--- src/components/WhiteGridBox/WhiteGridContainer.tsx
+++ src/components/WhiteGridBox/WhiteGridContainer.tsx
@@ -3,9 +3,11 @@ import WhiteGridBox from "./WhiteGridBox"
 
 const WhiteGridContainer = ({backgroundBlue=true}: {backgroundBlue:boolean}) => {
   return (
-    <div className={`${backgroundBlue && "lg:bg-bg-blue" } pb-15`}>
-      <ContentSection sectionLayout={["t"]} page={"scoutLife"} padding="top" background="large"/>
-      <div className={`flex flex-col lg:flex-row pt-4 items-center`}>
+    <div className={`${backgroundBlue && "lg:bg-bg-blue" } flex flex-col items-center pb-15`}>
+      <div className="w-full">
+        <ContentSection sectionLayout={["t"]} page={"scoutLife"} padding="top" background="large"/>
+      </div>
+      <div className={`flex flex-col lg:flex-row pt-4 w-full max-w-280 items-center`}>
         <WhiteGridBox logoUrl={"/shipIcon-desktop.svg"} messageTitle="scoutLife" messageTextIndex={"0"} backgroundWhite={backgroundBlue}/>
         <WhiteGridBox logoUrl={"/heartIcon-desktop.svg"} messageTitle="scoutLife" messageTextIndex={"1"} backgroundWhite={backgroundBlue}/>
         <WhiteGridBox logoUrl={"/shipIcon-desktop.svg"} messageTitle="scoutLife" messageTextIndex={"2"} backgroundWhite={backgroundBlue}/>
