e4c04fa153954f38771846f88a06084ac1b3d092
Maho Mikami <maho.cx1029@gmail.com>
2026-05-28 21:56:25 +0200

    fix navigation

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -32,9 +32,9 @@ const Navigation = ({ onClose }: NavigationProps) => {
     return (
         <div className={`absolute lg:static w-full bg-white text-primary rounded-b-lg font-albert tracking-[0.03em] top-16 md:top-29 lg:bg-primary lg:h-25.5 lg:text-text-white lg:rounded-none ${isScroll ? "lg:bg-primary/80" : ""}`}>
 
-            <div className="w-full lg:h-full lg:flex lg:justify-around lg:items-center lg:max-w-4xl lg:mx-auto xl:max-w-5xl 2xl:max-w-7xl">
+            <div className="w-full lg:h-full lg:flex lg:justify-around lg:items-center lg:max-w-4xl lg:mx-auto xl:max-w-5xl 2xl:max-w-400">
 
-                <div className="md:inline hidden pr-20 ">
+                <div className="lg:inline hidden xl:pr-20">
                     <Link href="https://www.scouterna.se/">
                         <Image src="/SWEScoutLogo.svg" alt="scoutlogo" width={30} height={30} className="opacity-70" />
                     </Link>
@@ -43,52 +43,53 @@ const Navigation = ({ onClose }: NavigationProps) => {
                 <div className="bg-bg-blue flex justify-between text-base h-69px px-4 py-4 font-bold border-b border-lightGray lg:hidden items-center">
                     <div>
                         <Link href="https://www.scouterna.se/" >
-                         <Image src="/blue-scoutLogo.svg" alt="scoutlogo" width={100} height={40} className="opacity-70 brightness-120" />
+                            <Image src="/blue-scoutLogo.svg" alt="scoutlogo" width={100} height={40} className="opacity-70 brightness-120" />
                         </Link>
                     </div>
-                    <LanguageSelector /> 
+                    <LanguageSelector />
                 </div>
-
-                {menuList.map((menu, index) => (
-                    <div key={index}>
-                        <div onClick={() => { handleToggle(menu.id); }}
-                            className="flex justify-between text-base  h-69px px-3 py-4 font-semibold border-b border-lightGray cursor-pointer lg:font-normal lg:border-none lg:static lg:hover:text-gray-300">
-                            <p> {messages?.navigation?.[menu.nameKey]} </p>
-                            <div className="relative w-7 h-7 lg:hidden">
-                                <AiOutlineDownCircle
-                                    size={28}
-                                    className={`absolute transform transition-transform duration-200 
+                {
+                    menuList.map((menu, index) => (
+                        <div key={index}>
+                            <div onClick={() => { handleToggle(menu.id); }}
+                                className="flex justify-between text-base  px-3 py-4 font-semibold border-b border-lightGray cursor-pointer lg:text-[18px] lg:font-normal lg:border-none lg:static lg:hover:text-gray-300">
+                                <p> {messages?.navigation?.[menu.nameKey]} </p>
+                                <div className="relative w-7 h-7 lg:hidden">
+                                    <AiOutlineDownCircle
+                                        size={28}
+                                        className={`absolute transform transition-transform duration-200 
                                             ${openId === menu.id ? "rotate-180 opacity-0" : "rotate-0  opacity-100"}`}
-                                />
-                                <AiFillUpCircle
-                                    size={28}
-                                    className={`lg:absolute transform transition-transform duration-200 
+                                    />
+                                    <AiFillUpCircle
+                                        size={28}
+                                        className={`lg:absolute transform transition-transform duration-200 
                                             ${openId === menu.id ? "rotate-180 opacity-100" : "rotate-0 opacity-0"}`}
-                                />
-                            </div>
-                            <div className="hidden lg:block ">
-                                <AiOutlineDown
-                                    className={`lg:ml-2 lg:mt-1.25 transform  transition-transform duration-200
+                                    />
+                                </div>
+                                <div className="hidden lg:block ">
+                                    <AiOutlineDown
+                                        className={`lg:ml-2 lg:mt-1.25 transform  transition-transform duration-200
                                         ${openId === menu.id ? "rotate-180" : "rotate-0"}`} />
-                            </div>
+                                </div>
 
-                        </div>
-                        {openId === menu.id && (
-                            <div onClick={() => { handleToggle(menu.id); onClose() }} className="px-4 py-4 border-b border-lightGray z-60 lg:absolute lg:py-0 lg:bg-white lg:text-primary lg:w-60 xl:w-85 lg:pt-0 lg:top-25.5 lg:border-none lg:shadow-md lg:rounded-b-2xl">
-                                {menu.submenu && menu.submenu.map((sub, index) => (
-                                    <Link href={messages?.path + sub.href} key={index} className="hover:text-text-gray">
-                                        <div key={index} className={`py-4 flex lg:h-18.5 cursor-pointer lg:items-center ${index !== menu.submenu.length - 1 ? 'lg:border-b lg:border-lightGray' : ''}`}>
-                                            <Image src={sub.iconBg} alt="image" width={44} height={44} className="w-11 h-auto" />
-                                            <div className="px-4 flex items-center ">
-                                                {messages?.navigation?.[sub.nameKey]}
-                                            </div>
-                                        </div>
-                                    </Link>
-                                ))}
                             </div>
-                        )}
-                    </div>
-                ))}
+                            {openId === menu.id && (
+                                <div onClick={() => { handleToggle(menu.id); onClose() }} className="px-4 py-4 border-b border-lightGray z-60 lg:absolute lg:py-0 lg:bg-white lg:text-primary lg:text-[18px] lg:w-60 xl:w-85 lg:pt-0 lg:top-25.5 lg:border-none lg:shadow-md lg:rounded-b-2xl">
+                                    {menu.submenu && menu.submenu.map((sub, index) => (
+                                        <Link href={messages?.path + sub.href} key={index} className="hover:text-text-gray">
+                                            <div key={index} className={`py-4 flex lg:h-18.5 cursor-pointer lg:items-center ${index !== menu.submenu.length - 1 ? 'lg:border-b lg:border-lightGray' : ''}`}>
+                                                <Image src={sub.iconBg} alt="image" width={44} height={44} className="w-11 h-auto" />
+                                                <div className="px-4 flex items-center ">
+                                                    {messages?.navigation?.[sub.nameKey]}
+                                                </div>
+                                            </div>
+                                        </Link>
+                                    ))}
+                                </div>
+                            )}
+                        </div>
+                    ))
+                }
                 <div className="rounded-b-lg p-4 flex flex-col lg:h-18.5 lg:items-center lg:hidden">
                     <p className="text-text-black font-bold uppercase text-xs pb-3">{messages?.navigation?.nav__service}</p>
                     {mobileMenuList && mobileMenuList.map((menu, index) => (
@@ -100,8 +101,8 @@ const Navigation = ({ onClose }: NavigationProps) => {
                         </Link>
                     ))}
                 </div>
-            </div>
-        </div>
+            </div >
+        </div >
     );
 }
 
