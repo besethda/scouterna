e32c481208ae0452ba29910e6fdde03a13baa0e4
Liting <janetli82@gmail.com>
2026-06-01 17:21:27 +0200

    fixing mobile size

--- src/components/AddCardAbout/index.tsx
+++ src/components/AddCardAbout/index.tsx
@@ -18,7 +18,7 @@ type pathType = {
 const CardAbout = ({ card, page, image, logo, MDlogo}: pathType) => {
   const messages = useMessages();
   return (
-    <>
+    <div>
       <div className="hidden md:block">
         <CardWithImage
           sectionTitle={page}
@@ -27,15 +27,17 @@ const CardAbout = ({ card, page, image, logo, MDlogo}: pathType) => {
         />
       </div>
       <div className="block md:hidden">
-        <CardWithoutImage
-          headline={messages?.[card]?.headline || ""}
-          logo={logo}
-          title={messages?.[card]?.title || ""}
-          text={messages?.[card]?.text || ""}
-          MDlogo={MDlogo}
-        />
+        <div className="-ml-2 sm:-ml-3 max-[345px]:ml-4 min-[412px]:-ml-10 min-[480px]:-ml-43 md:-ml-5"> 
+          <CardWithoutImage
+            headline={messages?.[card]?.headline || ""}
+            logo={logo}
+            title={messages?.[card]?.title || ""}
+            text={messages?.[card]?.text || ""}
+            MDlogo={MDlogo}
+          />
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
