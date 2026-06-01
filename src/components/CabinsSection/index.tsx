01d0dcc9283829e237054f2c1a989f0a5615220c
Liting <janetli82@gmail.com>
2026-06-01 14:25:41 +0200

    fix gap and text font size

--- src/components/CabinsSection/index.tsx
+++ src/components/CabinsSection/index.tsx
@@ -8,19 +8,19 @@ const CabinsSection = () => {
   const messages = useMessages()
   return(
     <div className="flex flex-col items-center md:gap-5 md:mb-20 ">
-      <div className="hidden md:block w-full max-w-4xl">
-        <h2 className=" text-primary text-3xl">{messages?.cabinst}</h2>
+      <div className="hidden md:block w-full max-w-6xl">
+        <h2 className=" text-primary text-h2-desktop">{messages?.cabinst}</h2>
       </div>
-      <div className="flex flex-col md:border md:border-lightBlue md:rounded-2xl bg-bg-blue w-full max-w-4xl md:h-auto lg:h-95">
+      <div className="flex flex-col md:border md:border-lightBlue md:rounded-2xl bg-bg-blue w-full max-w-6xl lg:h-90 ">
           <div className="flex px-5 gap-3 w-full h-full">
-            <div className="hidden md:flex items-start shrink-0 pt-6">
+            <div className="hidden md:flex items-start shrink-0 pt-6 ">
               <Image src="/blueHouseYellowBg.svg" alt="logo" width={44} height={44} className="h-10 w-auto" />
             </div>
-            <div className="flex flex-col flex-1 py-6">
-              <h3 className="text-primary text-body md:text-body-desktop">{messages?.cabinsmyset.title}</h3>
-              <p className="block md:hidden text-text-gray pt-2 text-body md:text-body-desktop">{messages?.cabinsmyset.p}</p>
-              <p className="hidden md:block text-text-gray pt-2 text-body md:text-body-desktop">{messages?.cabinsmyset.dpOne}</p>
-              <p className="hidden md:block text-text-gray pt-3 text-body md:text-body-desktop">{messages?.cabinsmyset.dpTwo}</p>
+            <div className="flex flex-col flex-1 py-6 mr-20">
+              <h3 className="text-primary text-h3-desktop">{messages?.cabinsmyset.title}</h3>
+              <p className="block md:hidden text-text-gray pt-2 md:text-body-desktop text-body">{messages?.cabinsmyset.p}</p>
+              <p className="hidden md:block text-text-gray pt-2 md:text-body-desktop text-body">{messages?.cabinsmyset.dpOne}</p>
+              <p className="hidden md:block text-text-gray pt-3 md:text-body-desktop text-body">{messages?.cabinsmyset.dpTwo}</p>
               <div className="mt-7">
                 <CTABtn text={messages?.mysetbtn}/>
               </div> 
@@ -30,16 +30,16 @@ const CabinsSection = () => {
             </div>  
           </div>
       </div>
-       <div className="flex flex-col md:border md:border-lightBlue md:rounded-2xl bg-bg-blue w-full max-w-4xl md:h-auto lg:h-95">
+       <div className="flex flex-col md:border md:border-lightBlue md:rounded-2xl bg-bg-blue w-full max-w-6xl lg:h-90">
           <div className="flex px-5 gap-3 w-full h-full">
             <div className="hidden md:flex items-start shrink-0 pt-6">
               <Image src="/blueHouseYellowBg.svg" alt="logo" width={44} height={44} className="h-10 w-auto" />
             </div>
-            <div className="flex flex-col flex-1 md:py-6 py-8">
-              <h3 className="text-primary text-body md:text-body-desktop">{messages?.cabinsruffen.title}</h3>
-              <p className="block md:hidden text-text-gray pt-2 text-body md:text-body-desktop">{messages?.cabinsruffen.p}</p>
-              <p className="hidden md:block text-text-gray pt-2 text-body md:text-body-desktop">{messages?.cabinsruffen.dpOne}</p>
-              <p className="hidden md:block text-text-gray pt-3 text-body md:text-body-desktop">{messages?.cabinsruffen.dpTwo}</p>
+            <div className="flex flex-col flex-1 md:py-6 py-8 mr-20">
+              <h3 className="text-primary text-h3-desktop">{messages?.cabinsruffen.title}</h3>
+              <p className="block md:hidden text-text-gray pt-2 md:text-body-desktop text-body">{messages?.cabinsruffen.p}</p>
+              <p className="hidden md:block text-text-gray pt-2 md:text-body-desktop text-body">{messages?.cabinsruffen.dpOne}</p>
+              <p className="hidden md:block text-text-gray pt-3 md:text-body-desktop text-body">{messages?.cabinsruffen.dpTwo}</p>
               <div className="mt-7">
                 <CTABtn text={messages?.ruffenbtn}/>
               </div> 
