903f6be0d6a3df936d704b4323908c4d602078ef
Liting <janetli82@gmail.com>
2026-06-02 08:21:50 +0200

    adding HSS-088-Activities

--- src/components/Activities/index.tsx
+++ src/components/Activities/index.tsx
@@ -7,16 +7,16 @@ import Img from '../../../public/images/IMG_8266.jpg'
 const Activities = () => {
   const messages =useMessages()
   return(
-    <div className="bg-bg-blue items-center px-43 py-15 hidden md:flex">
+    <div className="bg-bg-blue md:items-center md:px-43 py-20 px-3 flex flex-col md:flex-row">
       <div className="flex flex-col flex-1">
-         <h2 className="text-primary text-h2-desktop">{messages?.activity.title}</h2>
-         <h3 className="text-primary text-h3-desktop mt-3">{messages?.activity.sub_title_one}</h3>
-         <p className="text-text-black text-body-desktop">{messages?.activity.p_one}</p>
-         <h3 className="text-primary text-h3-desktop mt-3">{messages?.activity.sub_title_two}</h3>
-         <p className="text-text-black text-body-desktop">{messages?.activity.p_two}</p>
+         <h2 className="text-primary md:text-h2-desktop text-h2-albert">{messages?.activity.title}</h2>
+         <h3 className="text-primary md:text-h3-desktop mt-3 text-h3">{messages?.activity.sub_title_one}</h3>
+         <p className="text-text-black md:text-body-desktop">{messages?.activity.p_one}</p>
+         <h3 className="text-primary md:text-h3-desktop mt-3 text-h3">{messages?.activity.sub_title_two}</h3>
+         <p className="text-text-black md:text-body-desktop">{messages?.activity.p_two}</p>
       </div>
-      <div className="flex flex-1">
-        <Image src={Img} alt="img" className="rounded-2xl ml-45 max-w-100 object-cover"/>
+      <div className="flex flex-1 mt-5">
+        <Image src={Img} alt="img" className="rounded-2xl md:ml-45 md:max-w-100 object-cover"/>
       </div>
     </div>
   )
