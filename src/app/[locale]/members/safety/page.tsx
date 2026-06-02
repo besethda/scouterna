59582cb585ec0bbfc9749e410ba6538c64134e49
Sofia Runsten <runsten.sofia@gmail.com>
2026-06-02 10:52:58 +0200

    add missing p

--- src/app/[locale]/members/safety/page.tsx
+++ src/app/[locale]/members/safety/page.tsx
@@ -36,11 +36,11 @@ const Safety = ({params}:{params: Promise<{locale: string}>}) => {
                     </div>
                 </div>
                 <div className="flex flex-col w-full">
-                    <div className="order-1 md:order-2">
+                    <div className="order-1 md:order-2"> 
                         <ContentSection sectionLayout={["h", "w", "p", "t", "s", "p", "s", "p", "s", "p"]} page="safeScout" background={"mobile"} padding="both"/>
                     </div>
                     <div className="order-2 md:order-1">
-                        <ContentSection sectionLayout={["t", "p", "p", "t", "w", "l", "l", "l", "t", "p", "w", "l", "l", "l", "t", "w"]} background={"desktop"} page={"guidelines"} padding="top"/>
+                        <ContentSection sectionLayout={["t", "p", "p", "t", "p", "w", "l", "l", "l", "t", "p", "w", "l", "l", "l", "t", "w"]} background={"desktop"} page={"guidelines"} padding="top"/>
                     </div>
                     <div className="order-3">
                         <ContentSection sectionLayout={["h", "w", "p", "w"]} page="lockable" background="blue" display="desktop"/>
