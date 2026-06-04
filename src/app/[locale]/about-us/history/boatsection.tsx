25c5420f94f1f7059b994b94afdec34983951d41
Seth Armistead <sdarmistead@gmail.com>
2026-06-04 14:51:11 +0200

    git my code from main branch

--- src/app/[locale]/about-us/history/boatsection.tsx
+++ src/app/[locale]/about-us/history/boatsection.tsx
@@ -23,15 +23,17 @@ const BoatSection = ({photoArray}:{photoArray?:any[]}) => {
       mediaUrl: "/images/IMG_0411.JPEG",
     },
   ]
+  
+  const useableArray = photoArray && photoArray.length < 5 ? photoArray : fallbackData
+  const galleryPhotos: instaType[]= useableArray?.map((post: any, index:number)=> {
+    return {mediaUrl:post.mediaUrl, permalink: `link${index}`, mediaType: "image", caption:"boat picture", timestamp: (index*2), imageIndex:index}
+  })
 
-    const useableArray = photoArray && photoArray.length < 5 ? photoArray : fallbackData
-    const galleryPhotos: instaType[]= useableArray?.map((post: any, index:number)=> {
-
-  return (
-    <div className="bg-bg-white pt-6 md:pb-25 pb-15 px-6.5 w-full md:px-17 md:max-w-200 lg:max-w-430">
-      <Gallery photoObject={galleryPhotos} caption={true}/>
-    </div>
-  )
-})}
+return (
+  <div className="w-full bg-bg-white pb-16 pt-6">
+    <Gallery photoObject={galleryPhotos} caption={true}/>
+  </div>
+)
+}
 
 export default BoatSection
