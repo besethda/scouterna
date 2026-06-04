c7156139e788d56d6e2bdb292366c0b9fb66b933
Dobeen Kim <dobin7955@gmail.com>
2026-06-04 10:22:22 +0200

    chaged psdding

--- src/components/HeroContainer/HeroContainer.tsx
+++ src/components/HeroContainer/HeroContainer.tsx
@@ -27,7 +27,7 @@ const HeroContainer = ({ bgImages, messageSection, position }: { bgImages: { mob
       <CTABtn
         text={`${(messages as any)?.[`${messageSection}`]?.button}`}
         onClick={handleClick}
-        width="mobile" />
+        width="fit" />
     </HeroSection>
   )
 }
