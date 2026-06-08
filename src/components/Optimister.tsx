efc5e1c213d71609630e479a8d8db56f562f6c75
Dobeen Kim <dobin7955@gmail.com>
2026-06-08 11:43:46 +0200

    removed import

--- src/components/Optimister.tsx
+++ src/components/Optimister.tsx
@@ -2,7 +2,6 @@
 
 import { useEffect, useRef, useState } from 'react';
 import Image from 'next/image';
-import { transform } from 'next/dist/build/swc';
 
 interface OptimisterProps {
     title: string;
