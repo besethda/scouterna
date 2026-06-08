f46a579abc49cdb2b02b71b6521b76d454be8569
Maho Mikami <maho.cx1029@gmail.com>
2026-06-08 22:17:20 +0200

    fis scout page

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -31,12 +31,14 @@ const ImageCard = ({ imageTitle, image, section, page, text, pin }: ImageCardPro
         ${page === "home" && "w-89.5"}
         ${page === "cabin" && "w-66.5"}
         ${page === "group" && "w-81.5 md:w-87"}
+        ${page === "scout-life" && "w-89.5"}
         `}
         >
             <Image src={pinColor()} alt="hige" width={85} height={80} className="absolute inset-s-0 left-33 -top-4 z-40" />
             <div className={`mb-3.25  relative overflow-hidden
             ${page === "cabin" && "h-57.25"}
             ${page === "home" && "h-75"}
+            ${page === "scout-life" && "h-75"}
             `}>
                 <Image src={image} alt={imageTitle} fill className="object-cover  object-center" />
             </div>
