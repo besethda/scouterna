cb1058a3a188f640db6f888c0286bac884343b81
Maho Mikami <maho.cx1029@gmail.com>
2026-06-09 20:32:41 +0200

    fix

--- src/components/DivisionsContainer/DivisionsBox.tsx
+++ src/components/DivisionsContainer/DivisionsBox.tsx
@@ -3,7 +3,7 @@
 import Image from "next/image"
 import useMessages from "@/hook/useMessages"
 import Link from "next/link"
-import ImageCard from "../ImageCard"
+import ImageCard from "../ImageCard";
 import { urlFor } from "@/sanity/lib/image";
 
 type divisionName = "sjohumlorna" | "kaparna" | "utmanare" | "konvojen" | "smattarne"
@@ -20,7 +20,6 @@ const DivisionsBox = ({ division, imageUrl, data, boxStyle = "basic" }:
     smattarne: ["border-purple", "bg-purple-opaque"]
   }
   if (!data || data.groups.day_se) return null
-
 
   return (
     <div className={`${colorReference[division][0]} flex flex-col flex-wrap border border-b-4 m-1 rounded-2xl px-6 py-3 ${boxStyle !== "image" ? boxStyle === "info" ? "md:w-[49.5%] bg-bg-white mx-[.25%]" : "md:w-[24%] mx-[0.5%] md:min-w-77 md:m-3" : "md:min-h-85  md:max-h-96 lg:max-h-77 md:max-w-250 md:my-4"}`}>
