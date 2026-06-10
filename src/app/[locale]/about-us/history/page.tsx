8ac2444f5393896859393be7c14c3459cdabb35d
Liting <janetli82@gmail.com>
2026-06-10 19:44:15 +0200

    fix padding

--- src/app/[locale]/about-us/history/page.tsx
+++ src/app/[locale]/about-us/history/page.tsx
@@ -7,6 +7,9 @@ import { En } from "@/messages/en";
 import { Sv } from "@/messages/sv";
 import ContentSection from "@/components/ContentSection";
 import CardWithLogo from "@/components/CardWithLogo";
+import Image from "next/image";
+import line from "../../../../../public/historia-line.svg"
+
 
 
 const pageItem = "history"
@@ -23,23 +26,22 @@ const History = async ({ params }: { params: Promise<{ locale: string }> }) => {
     const messages = locale === "en" ? En : Sv
     const data = await getId("c57001b2-94f1-4bf1-ae81-1fcc684e0eee")
 
-
     return (
         <main>
             <Breadcrumbs />
             <CardWithLogo  image="/heartYellowBg.svg" sectionTitle="historycard" isH1 /> 
-            <div className="w-full "> 
-              <div className="flex justify-center bg-bg-blue pb-10">
-                <div className="relative md:max-w-[89%] max-w-[90%] rounded-3xl w-full shadow-xl bg-bg-white overflow-hidden px-8">
+            <div className="w-full mt-8 mb-10"> 
+              <div className="flex justify-center">
+                <div className="relative max-w-[92%] md:max-w-[95%]  xl:max-w-[89%] 2xl:max-w-350 rounded-3xl w-full shadow-xl bg-bg-white overflow-hidden md:px-10 px-3">
                   <h2 className="w-fit py-2 text-h2 md:text-h2-desktop text-primary font-fraunces font-bold">{locale === "sv" ? data?.history_section.title_se : data?.history_section.title_en}</h2>
-                  <img src="/Path.png" alt="line" className="absolute md:top-17 md:left-7 xl:left-5 top-10 left-7 w-[65%] md:w-auto line"/> 
+                  <img src="/Path.png" alt="line" className="absolute md:top-17 md:left-8 xl:left-5 top-10 left-3 w-[65%] md:w-auto line"/> 
                   <SanityFlex data={data?.history_layout.scout_life} locale={locale}/>
                 </div>
               </div>
             </div>
-            <ContentSection sectionLayout={["t"]} page={"boatSection"} padding={"top"} background={"blue"}/>
-            <ContentSection sectionLayout={["p"]} page={"boatSection"} display={"desktop"} padding={"none"} background={"blue"}/>
-            <BoatSection />
+            <ContentSection sectionLayout={["t"]} page={"boatSection"} padding={"top"} background={"blue"} />
+            <ContentSection sectionLayout={["p"]} page={"boatSection"} display={"desktop"} padding={"none"} background={"blue"} />
+            <BoatSection images={data?.images} />
         </main>
     );
 }
