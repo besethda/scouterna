68ed27648d7a217af03d893fffae03533e4ff549
Maho Mikami <maho.cx1029@gmail.com>
2026-06-10 17:36:49 +0200

    fix

--- src/components/Fragor-svar.tsx
+++ src/components/Fragor-svar.tsx
@@ -14,9 +14,9 @@ const FragorSvar = ({ question, answer }: FragorSvarProps) => {
     const [isOpen, setIseOpen] = useState<boolean>(false)
 
     return (
-        <div className="text-text-white w-full max-w-75 md:max-w-140 text-body md:text-body-desktop tracking-[0.03em] mx-auto">
+        <div className="text-text-white w-full max-w-75 md:max-w-140 text-body md:text-body-desktop tracking-[0.03em] mx-auto shadow-lg">
             <button onClick={() => setIseOpen(!isOpen)} className={`text-left flex font-albert text-body md:text-body-desktop flex-row gap-4 md:gap-8
-                    w-full bg-primary px-4 py-3.25 md:py-5.25 transition-all ${isOpen ? "rounded-t-lg" : "rounded-lg"}`}>
+                    w-full bg-primary px-4 py-3.25 md:py-5.25 transition-all shadow-lg ${isOpen ? "rounded-t-lg" : "rounded-lg"}`}>
                 <span dangerouslySetInnerHTML={{ __html: question }} className="flex-1 leading-normal md:leading-relaxed " />
                 <AiOutlineDownCircle className={`w-7 h-7 cursor-pointer transform transition-transform duration-200 
                 ${isOpen ? "rotate-180" : "rotate-0"}`} />
@@ -24,7 +24,7 @@ const FragorSvar = ({ question, answer }: FragorSvarProps) => {
             {isOpen && (
                 <div
                     className="bg-bg-white border border-primary rounded-b-lg rounded-t-none whitespace-pre-line border-t-0 text-text-black font-albert px-4 py-3.25 md:py-5.25
-                mt-0.5"
+                mt-0.5 shadow-lg"
                 ><PortableText components={{
                     block:
                         { normal: ({ children }) => <p className="mb-3">{children}</p> },
