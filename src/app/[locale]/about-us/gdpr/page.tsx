de42c48fd1efc5408502ea16411a25bafd112557
Liting <janetli82@gmail.com>
2026-06-10 14:01:27 +0200

    fix later

--- src/app/[locale]/about-us/gdpr/page.tsx
+++ src/app/[locale]/about-us/gdpr/page.tsx
@@ -23,7 +23,7 @@ const Gdpr = async ({params}:{params: Promise<{locale: string}>}) => {
         <main>
             <Breadcrumbs />
             <CardWithLogo image="/informationYellowBg.svg" sectionTitle="gdprcard" isH1/> 
-            <div className="flex justify-center bg-bg-blue pb-10">
+            {/* <div className="flex justify-center bg-bg-blue pb-10">
               <div className="md:max-w-[89%] max-w-[90%] rounded-3xl w-full shadow-xl bg-bg-white overflow-hidden">
                 <ContentSection sectionLayout={["t", "p", "s", "l", "l", "l", "l", "l", "s", "l", "l", "l", "l", "l", "l", "l", "l", "l", "p"]} page={"gdpr"} background={"white"}/>
               </div>
@@ -33,9 +33,10 @@ const Gdpr = async ({params}:{params: Promise<{locale: string}>}) => {
                 <ContentSection sectionLayout={["s", "p"]} page={"syfte"} background={"none"} />
               </div>
             </div>
-            <ContentSection sectionLayout={["s", "p", "p", "p", "p", "p", "p"]} page={"ethics"} display={"mobile"}/>
-            {/* <h2 className="w-fit py-2 text-h2 md:text-h2-desktop text-primary font-fraunces font-bold">{locale === "se" ? data?.gdpr_section.title_se : data?.gdpr_section.title_en}</h2>
-            <SanityFlex data={data?.history_layout.scout_life} locale={locale}/> */}
+            <ContentSection sectionLayout={["s", "p", "p", "p", "p", "p", "p"]} page={"ethics"} display={"mobile"}/> */}
+            <h2 className="w-fit py-2 text-h2 md:text-h2-desktop text-primary font-fraunces font-bold">{locale === "se" ? data?.gdpr_section.title_se : data?.gdpr_section.title_en}</h2>
+            <SanityFlex data={data?.history_layout.scout_life} locale={locale}/>
+
         </main>
     );
 }
