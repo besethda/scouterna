7179fdba7f2dd548b88d517b4f8273e32267b183
Seth Armistead <sdarmistead@gmail.com>
2026-06-10 22:54:56 +0200

    removed console.log

--- src/hook/useOutsideClick.ts
+++ src/hook/useOutsideClick.ts
@@ -4,7 +4,6 @@ import { RefObject, useEffect } from "react"
 
 export const useOutsideClick = (ref:RefObject<HTMLElement|null>, currentState:number|boolean|null, stateFalse:()=>void) => {
   useEffect(()=> {
-    console.log('click')
     const click=(click:MouseEvent)=> {
         if(ref.current && !ref.current.contains(click.target as Node)){
           stateFalse()
