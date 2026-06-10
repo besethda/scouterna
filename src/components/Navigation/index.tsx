bcec4336eff63b4932960edb3acfdf74706a5a3d
Maho Mikami <maho.cx1029@gmail.com>
2026-06-10 19:43:41 +0200

    fix nav

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -23,7 +23,7 @@ const Navigation = ({ onClose }: NavigationProps) => {
 
     useEffect(() => {
         const handleScroll = () => {
-        setIsScroll(window.scrollY > 200)
+            setIsScroll(window.scrollY > 200)
         }
 
         window.addEventListener("scroll", handleScroll)
@@ -49,7 +49,7 @@ const Navigation = ({ onClose }: NavigationProps) => {
                     </div>
                     <LanguageSelector />
                 </div>
-                    <div className="lg:flex lg:w-full lg:justify-evenly lg:max-w-300">
+                <div className="lg:flex lg:w-full lg:justify-evenly lg:max-w-300">
                     {menuList.map((menu, index) => (
                         <div key={index}>
                             <div onClick={() => { handleToggle(menu.id); }}
@@ -75,7 +75,7 @@ const Navigation = ({ onClose }: NavigationProps) => {
                             </div>
                             {openId === menu.id && (
                                 <div onClick={() => { handleToggle(menu.id); onClose() }} className={`px-4 py-4 border-b border-lightGray z-60 lg:absolute lg:py-0 lg:bg-white lg:text-primary lg:text-[18px] lg:w-70 xl:w-85 lg:pt-0 lg:top-25.5 lg:border-none lg:shadow-md lg:rounded-b-2xl 
-                                ${menu.id === 5 ? 'lg:right-2 right-auto-2000' : ''}`}>
+                                ${menu.id === 5 ? 'lg:right-2 right-auto-1560' : ''}`}>
                                     {menu.submenu && menu.submenu.map((sub, index) => (
                                         <Link href={messages?.path + sub.href} key={index} className="hover:text-text-gray">
                                             <div key={index} className={`py-4 flex lg:h-18.5 cursor-pointer lg:items-center ${index !== menu.submenu.length - 1 ? 'lg:border-b lg:border-lightGray' : ''}`}>
