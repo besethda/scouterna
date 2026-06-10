c778140ee3d064eb3c97bf8c8bf65be0146ea230
Maho Mikami <maho.cx1029@gmail.com>
2026-06-10 15:24:44 +0200

    padding check

--- src/app/[locale]/groups/page.tsx
+++ src/app/[locale]/groups/page.tsx
@@ -20,20 +20,20 @@ const GroupStructure = async ({ params }: { params: Promise<{ locale: string }> 
   const { locale } = await params
   const messages = locale === "en" ? En : Sv
   const data = await getId('2a5a4f80-b206-44b9-9e00-6b870a00f90e')
-  if(!data) return
+  if (!data) return
 
 
   return (
-    <main className="">
+    <main className="w-full md:max-w-360 mx-auto">
       <Breadcrumbs />
       <section className="w-full flex flex-col items-center">
-        <CardWithLogo image="/heartYellowBg.svg" sectionTitle="groupsCard" isH1/>
-        <div className="flex flex-col px-4 pb-8 md:pb-16 gap-1 items-center">
-          <DivisionsBox division={"sjohumlorna"} boxStyle={"image"} data={data?.groups.find((e:any) => e.name_slug === "sjohumlorna") || null}/>
-          <DivisionsBox division={"kaparna"} boxStyle={"image"} data={data?.groups.find((e:any) => e.name_slug === "kaparna") || null}/>
-          <DivisionsBox division={"konvojen"} boxStyle={"image"} data={data?.groups.find((e:any) => e.name_slug === "konvojen") || null}/>
-          <DivisionsBox division={"utmanare"} boxStyle={"image"} data={data?.groups.find((e:any) => e.name_slug === "utmanare") || null}/>
-          <DivisionsBox division={"smattarne"} boxStyle={"image"} data={data?.groups.find((e:any) => e.name_slug === "smattarne") || null} />
+        <CardWithLogo image="/heartYellowBg.svg" sectionTitle="groupsCard" isH1 />
+        <div className="flex flex-col px-4 pb-8 md:pb-16 gap-1 items-center lg:px-22">
+          <DivisionsBox division={"sjohumlorna"} boxStyle={"image"} data={data?.groups.find((e: any) => e.name_slug === "sjohumlorna") || null} />
+          <DivisionsBox division={"kaparna"} boxStyle={"image"} data={data?.groups.find((e: any) => e.name_slug === "kaparna") || null} />
+          <DivisionsBox division={"konvojen"} boxStyle={"image"} data={data?.groups.find((e: any) => e.name_slug === "konvojen") || null} />
+          <DivisionsBox division={"utmanare"} boxStyle={"image"} data={data?.groups.find((e: any) => e.name_slug === "utmanare") || null} />
+          <DivisionsBox division={"smattarne"} boxStyle={"image"} data={data?.groups.find((e: any) => e.name_slug === "smattarne") || null} />
         </div>
       </section>
     </main>
--- src/components/Fragor-svar.tsx
+++ src/components/Fragor-svar.tsx
@@ -1,36 +1,38 @@
 "use client"
 
-import {useState} from "react"
+import { useState } from "react"
 import { AiOutlineDownCircle } from "react-icons/ai";
 import { PortableText } from "next-sanity";
 
 interface FragorSvarProps {
-    question : string;
-    answer : string;
-} 
+    question: string;
+    answer: string;
+}
 
-const FragorSvar = ({question, answer}:FragorSvarProps) => {
+const FragorSvar = ({ question, answer }: FragorSvarProps) => {
 
     const [isOpen, setIseOpen] = useState<boolean>(false)
 
     return (
         <div className="text-text-white w-full max-w-75 md:max-w-140 text-body md:text-body-desktop tracking-[0.03em] mx-auto">
-                <button onClick={() => setIseOpen(!isOpen)} className={`text-left flex font-albert text-body md:text-body-desktop flex-row gap-4 md:gap-8
+            <button onClick={() => setIseOpen(!isOpen)} className={`text-left flex font-albert text-body md:text-body-desktop flex-row gap-4 md:gap-8
                     w-full bg-primary px-4 py-3.25 md:py-5.25 transition-all ${isOpen ? "rounded-t-lg" : "rounded-lg"}`}>
-                    <span dangerouslySetInnerHTML={{ __html: question }} className="flex-1 leading-normal md:leading-relaxed " />
-                    <AiOutlineDownCircle className={`w-7 h-7 cursor-pointer transform transition-transform duration-200 
+                <span dangerouslySetInnerHTML={{ __html: question }} className="flex-1 leading-normal md:leading-relaxed " />
+                <AiOutlineDownCircle className={`w-7 h-7 cursor-pointer transform transition-transform duration-200 
                 ${isOpen ? "rotate-180" : "rotate-0"}`} />
-                </button>
+            </button>
             {isOpen && (
-                <div 
-                className="bg-bg-white border border-primary rounded-b-lg rounded-t-none whitespace-pre-line border-t-0 text-text-black font-albert px-4 py-3.25 md:py-5.25
-                mt-0.5" 
-                ><PortableText components={{block: 
-                    {normal: ({children})=> <p className="mb-3">{children}</p>},
+                <div
+                    className="bg-bg-white border border-primary rounded-b-lg rounded-t-none whitespace-pre-line border-t-0 text-text-black font-albert px-4 py-3.25 md:py-5.25
+                mt-0.5"
+                ><PortableText components={{
+                    block:
+                        { normal: ({ children }) => <p className="mb-3">{children}</p> },
                     marks: {
-                    link: ({value, children})=> <a target="_blank" href={value.href} className="mb-3 underline hover:text-accent">{children}</a>
-                }}}
-                    value={answer as any}/></div>
+                        link: ({ value, children }) => <a target="_blank" href={value.href} className="mb-3 underline hover:text-accent">{children}</a>
+                    }
+                }}
+                    value={answer as any} /></div>
             )}
         </div>
     )
