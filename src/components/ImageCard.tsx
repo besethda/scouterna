28a13054d542fb84e1d5870178076316d62b7114
Maho Mikami <maho.cx1029@gmail.com>
2026-06-11 19:54:00 +0200

    save

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -25,15 +25,7 @@ const ImageCard = ({ imageTitle, image, section, page, text, pin }: ImageCardPro
     }
 
     return (
-        <div className={`flex flex-col  bg-bg-white shadow-[0_3px_8px_rgba(0,0,0,0.5)] relative font-gochi text-primary transition-transform duration-500 ease-out pin-swing
-        ${text ? "min-h-125" : ""}
-        ${page === "home" && "p-6 w-89.5"}
-        ${page === "cabin" && "p-6 w-66.5"}
-        ${page === "group" && "p-3 w-82 h-98.75 md:w-89.5"}
-        ${page === "groups" && "p-6 w-77.5 md:w-66.5 md:h-75.5"}
-        ${page === "scout-life" && "p-6 w-89.5 max-w-full"}
-        ${page === "join" && "p-6 w-82 md:w-89.5"}
-        `}>
+        <div className="relative group">
             <Image src={pinColor()} alt={imageTitle} width={85} height={80} className={`absolute inset-s-0 z-20 
             ${page === "cabin" || page === "groups"
                     ?
@@ -43,7 +35,23 @@ const ImageCard = ({ imageTitle, image, section, page, text, pin }: ImageCardPro
                         :
                         "left-33 -top-4"}
             `} />
-            <div className={`mb-3.25 relative overflow-hidden
+            <div className={`flex flex-col  bg-bg-white shadow-[0_3px_8px_rgba(0,0,0,0.5)] relative font-gochi text-primary transition-transform duration-500 ease-out pin-swing "origin-[calc(132px+42.5px)_0px
+        ${text ? "min-h-125" : ""}
+        ${page === "home" && "p-6 w-89.5"}
+        ${page === "cabin" && "p-6 w-66.5"}
+        ${page === "group" && "p-3 w-82 h-98.75 md:w-89.5"}
+        ${page === "groups" && "p-6 w-77.5 md:w-66.5 md:h-75.5"}
+        ${page === "scout-life" && "p-6 w-89.5 max-w-full"}
+        ${page === "join" && "p-6 w-82 md:w-89.5"}
+        ${page === "cabin" || page === "groups"
+                    ? "origin-[calc(80px+42.5px)_0px]"
+                    : page === "group"
+                        ? "origin-[calc(112px+42.5px)_0px] md:origin-[calc(132px+42.5px)_0px]"
+                        : "origin-[calc(132px+42.5px)_0px]"
+                }
+        `}>
+
+                <div className={`mb-3.25 relative overflow-hidden
             ${page === "cabin" && "h-57.25"}
             ${page === "home" && "h-75"}
             ${page === "scout-life" && "h-75"}
@@ -51,15 +59,16 @@ const ImageCard = ({ imageTitle, image, section, page, text, pin }: ImageCardPro
             ${page === "group" && "h-75"}
             ${page === "join" && "h-75"}
             `}>
-                <Image src={image} alt={imageTitle} fill className="object-cover  object-center" />
-            </div>
-            <div>
-                <p className="text-center text-hero">{(messages as any)[section]?.[imageTitle]}</p>
-            </div>
-            <div>
-                <p className="text-h5-desktop">{(messages as any)[section]?.[text]}</p>
-            </div>
+                    <Image src={image} alt={imageTitle} fill className="object-cover  object-center" />
+                </div>
+                <div>
+                    <p className="text-center text-hero">{(messages as any)[section]?.[imageTitle]}</p>
+                </div>
+                <div>
+                    <p className="text-h5-desktop">{(messages as any)[section]?.[text]}</p>
+                </div>
 
+            </div>
         </div>
 
     )
