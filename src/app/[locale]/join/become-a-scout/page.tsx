30cf98b905eff85dbf780f582f75e5d120035854
Sofia Runsten <runsten.sofia@gmail.com>
2026-06-11 17:48:01 +0200

    HSS-179-images

--- src/app/[locale]/join/become-a-scout/page.tsx
+++ src/app/[locale]/join/become-a-scout/page.tsx
@@ -14,6 +14,9 @@ import { getSectionById } from "@/lib/utils";
 import CardWithLogo from "@/components/CardWithLogo";
 import Line from "@/components/line"
 import Scoutlife from "@/components/Scoutlife";
+import boat from '../../../../../public/blueBoat1.svg'
+import heart from '../../../../../public/blueHeart1.svg'
+import Image from "next/image";
 
 const pageItem = "become-a-scout"
 const headDescription = "become-a-scout"
@@ -44,13 +47,14 @@ const BecomeAScout = ({ params }: { params: Promise<{ locale: string }> }) => {
                         <SmallCard title={messages?.becomeScoutSmallCard?.title} subtitle={messages?.becomeScoutSmallCard?.subtitle} secondTitle={messages?.becomeScoutSmallCard?.secondTitle} secondText={messages?.becomeScoutSmallCard?.secondText} thirdtext={messages?.becomeScoutSmallCard?.thirdtext} button={messages?.becomeScoutSmallCard?.button} />
                         <Line hasPadding />
                         <section className="flex flex-col gap-6 font-albert px-4 pb-8 lg:max-w-430 lg:px-10">
-                            <div className="flex flex-col gap-4 ">
+                            <div className="relative **:flex flex-col gap-4 ">
                                 <h2 className="text-h2 font-fraunces font-bold text-primary md:text-h3-desktop">
                                     {messages?.joinContent?.title}
                                 </h2>
                                 <p className="text-text-black text-body lg:max-w-[72%] md:text-body-desktop tracking-[3%]">
                                     {messages?.joinContent?.text}
                                 </p>
+                                <Image src={boat} alt="birds" height={150} width={150} className="hidden lg:block lg:absolute lg:-top-50 lg:right-10 opacity-90 lg:rotate-12" />
                             </div>
                             <a
                                 href="/pdfs/Bilaga-20251012-HSS-Trygga-maten-policy-Beslutad.pdf"
@@ -68,13 +72,14 @@ const BecomeAScout = ({ params }: { params: Promise<{ locale: string }> }) => {
                                 <CTABtn text={messages?.joinContent?.button02} />
                             </a>
                             <Line />
-                            <div className="flex flex-col gap-6">
+                            <div className="relative flex flex-col gap-6">
                                 <h3 className="font-fraunces text-h2 font-bold text-[#00355F]  md:text-h3-desktop">
                                     {messages?.joinContent?.title02}
                                 </h3>
                                 <p className="text-text-black pb-4 text-body lg:max-w-[72%] md:text-body-desktop tracking-[3%]">
                                     {messages?.joinContent?.text03}
                                 </p>
+                                <Image src={heart} alt="birds" height={150} width={150} className="hidden lg:block lg:absolute lg:top-5 lg:right-10 opacity-80 lg:rotate-12" />
                             </div>
                         </section>
                     </div>
--- src/app/[locale]/what-we-do/cabins/ruffen/page.tsx
+++ src/app/[locale]/what-we-do/cabins/ruffen/page.tsx
@@ -7,6 +7,8 @@ import { getPageHeadTitle } from "@/lib/utils"
 import Breadcrumbs from "@/components/Breadcrumbs";
 import { getSectionById, getId } from "@/lib/utils";
 import CardWithLogo from "@/components/CardWithLogo";
+import birds from '../../../../../../public/blueBirds1.png'
+import Image from "next/image";
 
 
 
@@ -23,25 +25,26 @@ const Ruffen = async ({ params }: { params: Promise<{ locale: string }> }) => {
     const messages = locale === "en" ? En : Sv
     const data = await getSectionById('17520090-02e5-4b1c-b8e0-af8801314244', "cabin_images")
     const groupData = await getId('2a5a4f80-b206-44b9-9e00-6b870a00f90e')
-    
+
 
     return (
         <main>
             <Breadcrumbs />
             <div className="flex flex-col items-center w-full">
-                <CardWithLogo image="/cabinYellowBg.svg" sectionTitle="ruffen" isH1 logoTopRight/>
+                <CardWithLogo image="/cabinYellowBg.svg" sectionTitle="ruffen" isH1 logoTopRight />
                 <section className="md:bg-bg-blue py-7 flex w-full flex-col items-center">
                     <ContentSection sectionLayout={["t", "p"]} page={"ruffenDivisions"} padding="none" />
                     <div className="flex flex-col p-4 gap-1 md:px-25 md:grid md:grid-cols-2 w-full lg:max-w-430 lg:px-22 md:gap-2 [&>*]:w-full">
-                        <DivisionsBox division={"spararscouter"} boxStyle={"info"} data={groupData?.groups.find((e:any) => e.name_slug === "sjohumlorna") || null}/>
-                        <DivisionsBox division={"upptackarscouter"} boxStyle={"info"} data={groupData?.groups.find((e:any) => e.name_slug === "kaparna") || null}/>
-                        <DivisionsBox division={"utmanarscouter"} boxStyle={"info"} data={groupData?.groups.find((e:any) => e.name_slug === "utmanare") || null}/>
-                        <DivisionsBox division={"aventyrarscouter"} boxStyle={"info"} data={groupData?.groups.find((e:any) => e.name_slug === "konvojen") || null}/>
-                        <DivisionsBox division={"familjescouter"} boxStyle={"info"} data={groupData?.groups.find((e:any) => e.name_slug === "smattarne") || null}/>
+                        <DivisionsBox division={"spararscouter"} boxStyle={"info"} data={groupData?.groups.find((e: any) => e.name_slug === "sjohumlorna") || null} />
+                        <DivisionsBox division={"upptackarscouter"} boxStyle={"info"} data={groupData?.groups.find((e: any) => e.name_slug === "kaparna") || null} />
+                        <DivisionsBox division={"utmanarscouter"} boxStyle={"info"} data={groupData?.groups.find((e: any) => e.name_slug === "utmanare") || null} />
+                        <DivisionsBox division={"aventyrarscouter"} boxStyle={"info"} data={groupData?.groups.find((e: any) => e.name_slug === "konvojen") || null} />
+                        <DivisionsBox division={"familjescouter"} boxStyle={"info"} data={groupData?.groups.find((e: any) => e.name_slug === "smattarne") || null} />
                     </div>
                 </section>
-                <section className="pb-8 md:pt-8 md:pb-20 px-4 lg:max-w-430 lg:px-22 w-full">
+                <section className="relative pb-8 md:pt-8 md:pb-20 px-4 lg:max-w-430 lg:px-22 w-full">
                     <ContentSection sectionLayout={["t", "p", "p"]} page={"map"} padding="top" child={true} />
+                    <Image src={birds} alt="birds" height={150} width={150} className="hidden lg:block lg:absolute lg:top-0 lg:right-50 opacity-90 lg:rotate-20" />
                     <div className="">
                         <MapWrapper page="ruffen" />
                     </div>
