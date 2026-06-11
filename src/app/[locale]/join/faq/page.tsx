f32a248b176c7d827e8a2b2eddf07e54870ad49b
Seth Armistead <sdarmistead@gmail.com>
2026-06-12 01:37:48 +0200

    fixed faq

--- src/app/[locale]/join/faq/page.tsx
+++ src/app/[locale]/join/faq/page.tsx
@@ -26,7 +26,7 @@ const Faq = async ({ params }: { params: Promise<{ locale: string }> }) => {
     const data = await getId("44126518-6c9f-4c34-bc46-bea246cd70ca")
 
     return (
-        <main className="bg-bg-blue max-w-360 mx-auto">
+        <main className="bg-bg-blue max-w-360 overflow-hidden mx-auto">
             <Breadcrumbs />
             <div className="flex w-full flex-col items-center ">
                 <CardWithLogo image="/informationYellowBg.svg" sectionTitle="faqCard" isH1 />
@@ -52,9 +52,7 @@ const Faq = async ({ params }: { params: Promise<{ locale: string }> }) => {
                     <Image src={anchor} alt="birds" height={150} width={150} className="hidden lg:block lg:absolute lg:top-280 lg:left-10 opacity-90 lg:rotate-12" />
                 </div>
                 <div className="flex flex-col md:flex-row w-full pt-5 px-4 pb-10 lg:max-w-430 lg:px-22 md:flex md:pt-12.5 md:pb-16 md:items-start md:gap-25 items-center">
-                    <section className="flex flex-col gap-6 font-albert md:gap-4 w-full
-                    bg-white mx-4 mb-8 px-4 py-8 rounded-3xl md:px-10 lg:my-20 lg:max-w-430 lg:justify-between lg:w-full lg:relative shadow-xl
-                ">
+                    <section className="flex flex-col font-albert md:gap-4 w-full bg-white mb-8 px-4 py-8 rounded-3xl md:px-10 lg:my-20  lg:justify-between lg:w-full lg:relative shadow-xl">
                         <div className="flex flex-col w-full lg:w-150 lg:shrink-0 ">
                             <div className="flex justify-between lg:static">
                                 <h2 className="font-fraunces font-bold pb-2 text-h2 text-primary md:text-h2-desktop">
@@ -68,7 +66,7 @@ const Faq = async ({ params }: { params: Promise<{ locale: string }> }) => {
                                 {messages?.faqContact?.text}
                             </p>
                         </div>
-                        <div className="flex items-center gap-3 w-full lg:max-w-430">
+                        <div className="flex items-center gap-3 w-full">
                             <Image src="/msgIcon.svg" alt="logo" width={44} height={44} className="" />
                             <a href="mailto:info@hss-scout.org"
                                 className="text-text-black text-body md:text-body-desktop hover:underline font-bold
