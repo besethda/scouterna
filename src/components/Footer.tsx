db39ea021d45e019d094c91ae7a073651804309c
Liting <janetli82@gmail.com>
2026-06-11 09:11:51 +0200

    fix mobile footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -24,10 +24,10 @@ const Footer = () => {
 
     return (
         <footer className="bg-primary text-white py-6 px-5 flex flex-col gap-4 xl:py-20 xl:px-2.5 xl:gap-16.5 ">
-            <div className='flex flex-col gap-2.5 xl:flex xl:flex-row xl:justify-around xl:w-[90%] xl:mx-auto max-w-360 mx-auto'>
+            <div className='flex flex-col gap-2.5 xl:flex xl:flex-row xl:justify-around xl:w-[90%] xl:mx-auto md:max-w-360 w-90'>
                 <div className='xl:flex-1'>
                     <Link href="https://www.scouterna.se/">
-                        <Image src={scautLogo} alt="Scaut-logo" className='w-32.5 h-7 xl:h-[28]' />
+                        <Image src={scautLogo} alt="Scaut-logo" className='w-32.5 h-7 xl:h-[28] -ml-1.5' />
                     </Link>
                     <div className='pt-3'>
                         <p className="text-body xl:hidden">Hässelby Strands Sjöscouter</p>
