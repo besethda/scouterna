218a1cf0f8a00e04c94f107cf2ad4756a7b771bb
Seth Armistead <sdarmistead@gmail.com>
2026-06-11 21:41:37 +0200

    added click functions and better error handling

--- src/components/Optimister.tsx
+++ src/components/Optimister.tsx
@@ -29,22 +29,28 @@ const Optimister = ({ title, text, images, locale }: OptimisterProps) => {
         container?.scrollBy({left: (forward ? 340 : -340), behavior: 'smooth'})
     }
 
-    const changePhoto = () => {
+    const forwardPhoto = (click:React.MouseEvent, index:number) => {
+        click.stopPropagation()
+        images.length > index ? setCurrentImage(index) : setCurrentImage(0)
+    }
 
+    const backPhoto = (click:React.MouseEvent, index:number) => {
+        click.stopPropagation()
+        index === -1 ? setCurrentImage(images.length - 1) : setCurrentImage(index)
     }
 
     return (
         <div className='w-full relative pb-6 pt-6 lg:max-w-430 lg:px-22 px-4 overflow-hidden'>
-            {galleryOpen && <div onClick={()=> setGalleryOpen(false)} className='fixed w-screen h-screen bg-gray-800/50 backdrop-blur-sm top-0 left-0 z-70 flex items-center justify-center overscroll-contain overflow-hidden'>
-                {currentImage !== null && <div className='h-[80%] w-[80%] overflow-hidden relative '>
+            {galleryOpen && <div onClick={()=> setGalleryOpen(false)} className='fixed hidden w-screen h-screen bg-gray-800/50 backdrop-blur-sm top-0 left-0 z-70 md:flex items-center justify-center overscroll-contain overflow-hidden'>
+                {currentImage !== null && <div className='h-[80%] md:w-[80%] w-75% overflow-hidden relative '>
                     <Image src={urlFor(images[currentImage].asset._ref).url()} alt={images[currentImage].alt} fill className='object-contain'/>
                 </div>}
                 <div className='absolute w-full h-full flex items-center justify-between lg:px-8'>
                     <div  className='md:w-20 w-12 h-full flex items-center justify-center'>
-                        <svg onClick={()=> move(true)} className="pointer-events-auto active:drop-shadow-none drop-shadow-lg hover:scale-105 -rotate-90 duration-100 fill-text-white active:fill-text-gray/90 active:scale-95 cursor-pointer" width="30" height="30" viewBox="144 144 512 512" xmlns="http://www.w3.org/2000/svg"><path d="m399.44 263.41 189.21 221.68c10.078 11.195 8.3984 29.668-2.8008 39.184-11.195 9.5156-29.668 8.3984-39.184-2.8008l-147.22-170.73-147.22 170.73c-10.078 11.195-27.988 12.875-39.184 2.8008-11.195-10.078-12.875-27.988-2.8008-39.184z"/></svg>
+                        <svg onClick={(e)=> backPhoto(e, (currentImage !==null ? currentImage - 1 : 0))} className="pointer-events-auto active:drop-shadow-none drop-shadow-lg hover:scale-105 -rotate-90 duration-100 fill-text-white active:fill-text-gray/90 active:scale-95 cursor-pointer" width="30" height="30" viewBox="144 144 512 512" xmlns="http://www.w3.org/2000/svg"><path d="m399.44 263.41 189.21 221.68c10.078 11.195 8.3984 29.668-2.8008 39.184-11.195 9.5156-29.668 8.3984-39.184-2.8008l-147.22-170.73-147.22 170.73c-10.078 11.195-27.988 12.875-39.184 2.8008-11.195-10.078-12.875-27.988-2.8008-39.184z"/></svg>
                     </div>
                     <div className='md:w-20 w-12 h-full flex items-center justify-center'>
-                        <svg onClick={()=> move(true)} className="pointer-events-auto active:drop-shadow-none drop-shadow-lg hover:scale-105 rotate-90 duration-100 fill-text-white active:fill-text-gray/90 active:scale-95 cursor-pointer" width="30" height="30" viewBox="144 144 512 512" xmlns="http://www.w3.org/2000/svg"><path d="m399.44 263.41 189.21 221.68c10.078 11.195 8.3984 29.668-2.8008 39.184-11.195 9.5156-29.668 8.3984-39.184-2.8008l-147.22-170.73-147.22 170.73c-10.078 11.195-27.988 12.875-39.184 2.8008-11.195-10.078-12.875-27.988-2.8008-39.184z"/></svg>
+                        <svg onClick={(e)=> forwardPhoto(e, (currentImage !==null ? currentImage + 1 : 0))} className="pointer-events-auto active:drop-shadow-none drop-shadow-lg hover:scale-105 rotate-90 duration-100 fill-text-white active:fill-text-gray/90 active:scale-95 cursor-pointer" width="30" height="30" viewBox="144 144 512 512" xmlns="http://www.w3.org/2000/svg"><path d="m399.44 263.41 189.21 221.68c10.078 11.195 8.3984 29.668-2.8008 39.184-11.195 9.5156-29.668 8.3984-39.184-2.8008l-147.22-170.73-147.22 170.73c-10.078 11.195-27.988 12.875-39.184 2.8008-11.195-10.078-12.875-27.988-2.8008-39.184z"/></svg>
                     </div>
                 </div>
             </div>}
@@ -67,7 +73,7 @@ const Optimister = ({ title, text, images, locale }: OptimisterProps) => {
                             <div
                                 key={index}
                                 onClick={()=>{setGalleryOpen(true); setCurrentImage(index)}}
-                                className='w-75 h-52 cursor-pointer mx-2 snap-center shrink-0 relative overflow-hidden rounded-2xl 
+                                className='w-75 h-52 md:cursor-pointer mx-2 snap-center shrink-0 relative overflow-hidden rounded-2xl 
                                 lg:snap-none'
                             >
                                 <Image
