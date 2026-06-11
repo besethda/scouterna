1030c85c76587adb259446f5eee3b46b83cb4279
Dobeen Kim <dobin7955@gmail.com>
2026-06-11 20:10:25 +0200

    save

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -51,7 +51,7 @@ const ContactForm = ({ title, text, lastName, firstName, email, message, buttonT
     };
 
     return (
-        <div className="py-10 flex flex-col gap-2 font-albert lg:py-25 lg:max-w-430  px-4 lg:px-22 2xl:mx-auto relative">
+        <div className="py-10 flex flex-col gap-2 font-albert lg:py-25 lg:max-w-430  px-4 lg:px-22 2xl:mx-auto relative ">
             <Toaster position="top-center" reverseOrder={false} />
             <h2 className="text-h2 w-full font-fraunces font-bold text-primary md:text-h2-desktop">{title}</h2>
             <p className="text-body w-full text-text-black md:text-body-desktop pb-2 md:pb-8 max-w-70 md:max-w-100 lg:max-w-150">{text}</p>
