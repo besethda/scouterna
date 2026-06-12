07936554a8f98c93c6202e78144f97cc2cf3fb30
Sofia Runsten <runsten.sofia@gmail.com>
2026-06-12 18:13:55 +0200

    ruffen

--- src/app/[locale]/what-we-do/cabins/ruffen/page.tsx
+++ src/app/[locale]/what-we-do/cabins/ruffen/page.tsx
@@ -9,7 +9,7 @@ import { getSectionById, getId } from "@/lib/utils";
 import CardWithLogo from "@/components/CardWithLogo";
 import birds from '../../../../../../public/blueBirds1.png'
 import Image from "next/image";
-
+import boat from '../../../../../../public/blueBoat1.svg'
 
 
 const pageItem = "ruffen"
@@ -31,15 +31,21 @@ const Ruffen = async ({ params }: { params: Promise<{ locale: string }> }) => {
         <main className="w-full mx-auto lg:max-w-[var(--max-w-laptop)] 2xl:max-w-[var(--max-w-desktop)]">
             <Breadcrumbs />
             <div className="flex flex-col items-center w-full">
-                <CardWithLogo image="/cabinYellowBg.svg" sectionTitle="ruffen" isH1 logoTopRight/>
+                <CardWithLogo image="/cabinYellowBg.svg" sectionTitle="ruffen" isH1 logoTopRight />
+                <div className="relative bg-white rounded-2xl shadow-xl mb-12.5 mx-4 md:mx-0 ">
+                    <ContentSection sectionLayout={["t", "p", "t", "p", "s", "p", "p", "p"]} page="aboutRuffen" padding="both" />
+                    <Image src={birds} alt="birds" height={150} width={150} className="-top-10 right-0 md:block absolute md:top-15 md:right-10 opacity-90 " />
+                    <Image src={boat} alt="boat" height={150} width={150} className="hidden md:block md:absolute md:bottom-15 md:right-40 opacity-90 " />
+                                     
+                </div>
                 <section className="md:bg-bg-blue flex w-full flex-col items-center">
                     <ContentSection sectionLayout={["t", "p"]} page={"ruffenDivisions"} child={false} padding="none" />
                     <div className="flex flex-col px-4 lg:px-0 gap-1 md:grid md:grid-cols-2 w-full lg:max-w-430 md:gap-2 [&>*]:w-full">
-                        <DivisionsBox division={"spararscouter"} boxStyle={"info"} data={groupData?.groups.find((e:any) => e.name_slug === "sjohumlorna") || null}/>
-                        <DivisionsBox division={"upptackarscouter"} boxStyle={"info"} data={groupData?.groups.find((e:any) => e.name_slug === "kaparna") || null}/>
-                        <DivisionsBox division={"utmanarscouter"} boxStyle={"info"} data={groupData?.groups.find((e:any) => e.name_slug === "utmanare") || null}/>
-                        <DivisionsBox division={"aventyrarscouter"} boxStyle={"info"} data={groupData?.groups.find((e:any) => e.name_slug === "konvojen") || null}/>
-                        <DivisionsBox division={"familjescouter"} boxStyle={"info"} data={groupData?.groups.find((e:any) => e.name_slug === "smattarne") || null}/>
+                        <DivisionsBox division={"spararscouter"} boxStyle={"info"} data={groupData?.groups.find((e: any) => e.name_slug === "sjohumlorna") || null} />
+                        <DivisionsBox division={"upptackarscouter"} boxStyle={"info"} data={groupData?.groups.find((e: any) => e.name_slug === "kaparna") || null} />
+                        <DivisionsBox division={"utmanarscouter"} boxStyle={"info"} data={groupData?.groups.find((e: any) => e.name_slug === "utmanare") || null} />
+                        <DivisionsBox division={"aventyrarscouter"} boxStyle={"info"} data={groupData?.groups.find((e: any) => e.name_slug === "konvojen") || null} />
+                        <DivisionsBox division={"familjescouter"} boxStyle={"info"} data={groupData?.groups.find((e: any) => e.name_slug === "smattarne") || null} />
                     </div>
                 </section>
                 <section className="pb-10 md:pt-6 md:pb-16 px-4 lg:px-[22px] w-full 2xl:px-0 ">
--- src/messages/sv.ts
+++ src/messages/sv.ts
@@ -293,6 +293,16 @@ type MessagesSvType = {
     title: string,
     text: string,
   },
+  aboutRuffen: {
+    t0: string,
+    p0: string,
+    t1: string,
+    p1: string,
+    s0: string, 
+    p2: string, 
+    p3: string, 
+    p4: string,
+  }
   memberInfo: {
     headline: string,
     title: string,
@@ -979,6 +989,16 @@ export const Sv: MessagesSvType = {
     headline: "Scoutstugor",
     title: "Ruffen",
     text: "Ruffen är vår lokal vid Hässelby Strandbad. Här har vi våra avdelningsmöten och den huserar även våra optimistjollar."
+  },
+  aboutRuffen: {
+    t0: "Scöscouting sedan 1959",
+    p0: "Precis där Mälarens vågor möter land ligger Ruffen, den anrika scoutstugan som sedan 1959 har varit en viktig del Hässelby Strands Sjöscouter.",
+    t1: "Med sjön som närmaste granne",
+    p1: "Med sitt fantastiska läge är Ruffen den perfekta basen för sjöliv. Härifrån är steget aldrig långt till båtar, bryggor och glittrande vatten. Stugan fungerar som kårens naturliga samlingslokal där generationer av barn och ungdomar har mötts för att lära sig segling, knopar och sjövett, samt förbereda sig inför spännande äventyr.",
+    s0: "Kort om Ruffen:", 
+    p2: "Hemvist för Hässelby Strands Sjöscouter", 
+    p3: "Aktivt sedan 1959", 
+    p4: "Nära båtar och bryggor vid vattnet i Hässelby Strand, "
   },
   memberInfo: {
     headline: "Scoutliv",
