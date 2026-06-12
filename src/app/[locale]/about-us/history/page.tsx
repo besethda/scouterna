4468766d18a3d3a3c14e48442b83b8911637e904
Liting <janetli82@gmail.com>
2026-06-12 19:00:53 +0200

    fix later

--- src/app/[locale]/about-us/history/page.tsx
+++ src/app/[locale]/about-us/history/page.tsx
@@ -35,7 +35,7 @@ const History = async ({ params }: { params: Promise<{ locale: string }> }) => {
         <div className="flex justify-center">
             <img src="/Path.png" alt="line" className="absolute md:top-28 md:left-[16vw]  top-22 left-4 w-[65%] md:w-auto line" />
               {data?.history_layout.scout_life.map((e:any, index:number)=> {return(
-                <div key={index} className={`rounded-3xl rounded-3xl w-full shadow-xl ${index%2 === 0 ? "bg-bg-white" : "bg-primary"} my-8 overflow-hidden px-4 md:px-10 py-3`}>
+                <div key={index} className={`rounded-3xl w-full shadow-xl ${index%2 === 0 ? "bg-bg-white" : "bg-primary"} my-8 overflow-hidden px-4 md:px-10 py-3`}>
                   <SanityFlex data={e.section_array} locale={locale} color={index%2 === 0 ? "black" : "white"} child={true}/>
                 </div>
                 )})}   
