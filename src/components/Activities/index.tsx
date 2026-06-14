7af2f22045da8b85a081e5a4dfbf96165b50b9d2
Liting <janetli82@gmail.com>
2026-06-14 11:32:17 +0200

    fix activity and email padding

--- src/components/Activities/index.tsx
+++ src/components/Activities/index.tsx
@@ -7,7 +7,7 @@ import birds from '../../../public/blueBirds1.png'
 const Activities = () => {
   const messages = useMessages()
   return (
-    <div className="flex flex-col items-center bg-bg-white px-4 py-8 mx-4 mb-10 rounded-3xl md:mb-20 lg:max-w-[var(--max-w-laptop)] xl:mx-auto 2xl:max-w-[var(--max-w-desktop)]">
+    <div className="flex flex-col items-center bg-bg-white lg:px-10 px-4 py-8 mx-4 mb-10 rounded-3xl md:mb-20 lg:max-w-[var(--max-w-laptop)] xl:mx-auto 2xl:max-w-[var(--max-w-desktop)]">
       <div className=" w-full flex flex-col ">
         <div className="flex flex-col gap-6 max-w-[603px]">
           <h2 className="text-primary md:text-h2-desktop font-fraunces font-bold text-h2-albert">{messages?.activity.title}</h2>
