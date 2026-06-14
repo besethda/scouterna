e6da7528b345bfc55d5b788c4d7432a86a1c6268
Dobeen Kim <dobin7955@gmail.com>
2026-06-14 10:34:16 +0200

    fixed history line

--- src/app/[locale]/about-us/history/page.tsx
+++ src/app/[locale]/about-us/history/page.tsx
@@ -29,7 +29,7 @@ const History = async ({ params }: { params: Promise<{ locale: string }> }) => {
       <CardWithLogo image="/heartYellowBg.svg" sectionTitle="historycard" isH1 />
       <div className="w-full relative">
         <div className="relative flex justify-center">
-            <Image src="/Path.png" alt="line" width={300} height={20} className="absolute md:top-28 md:left-10 w-50 top-22 left-8  md:w-auto " />
+            <Image src="/Path.png" alt="line" width={300} height={20} className="absolute w-[204px] top-22 left-4 md:top-28 md:left-10 md:w-[386px] " />
               {data?.history_layout.scout_life.map((e:any, index:number)=> {return(
                 <div key={index} className={`rounded-3xl rounded-3xl w-full shadow-xl ${index%2 === 0 ? "bg-bg-white" : "bg-primary"} my-8 overflow-hidden px-4 md:px-10 py-3`}>
                   <SanityFlex data={e.section_array} locale={locale} color={index%2 === 0 ? "black" : "white"}/>
