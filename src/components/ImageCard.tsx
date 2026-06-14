cf034beff19060bcba4aea8f6acc88658f0f8805
Dobeen Kim <dobin7955@gmail.com>
2026-06-14 15:13:19 +0200

    save

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -32,10 +32,12 @@ const ImageCard = ({ imageTitle, image, section, page, text, pin }: ImageCardPro
                     "left-25 -top-5 md:left-20 md:-top-2"
                     : page === "cabin" ?
                         "left-20 -top-5 md:left-20 md:-top-2"
-                        : page === "group" || page === "scout-life"
-                            ? "left-22 -top-2 md:left-33"
-                            :
-                            "left-27 -top-2 md:left-33 md:-top-4"}
+                        : page === "scout-life" ?
+                            "left-22 -top-2 md:left-33"
+                            : page === "group"
+                                ? "left-25 -top-2 md:left-33"
+                                :
+                                "left-27 -top-2 md:left-33 md:-top-4"}
             `} />
             <div className={`flex flex-col  bg-bg-white shadow-[0_3px_8px_rgba(0,0,0,0.5)] relative font-gochi text-primary transition-transform duration-500 ease-out pin-swing
         ${text ? " md:min-h-125" : ""}
