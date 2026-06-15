14da1c9e6a6ed24eb0558169ddad1eb1a12e5457
Maho Mikami <maho.cx1029@gmail.com>
2026-06-15 12:50:19 +0200

    fix icons and sizing

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,9 +7,9 @@ import emailIcon from '../../public/email-icon.svg'
 import instagramIcon from '../../public/instagram-icon.svg'
 import keyIcon from '../../public/key-icon.svg'
 import bagIcon from '../../public/bag-icon.png'
-import schoolIcon from '../../public/school-icon.png'
+import schoolIcon from '../../public/book-icon.svg'
 import footerHSSIcon from '../../public/footer-hss-logo.png'
-import locationIcon from '../../public/hss-pin-icon.svg'
+import locationIcon from '../../public/yellow-location.svg'
 import useMessages from '@/hook/useMessages'
 import MapWrapper from './Map/MapWrapper'
 import { Facebook } from "@deemlol/next-icons"
@@ -33,17 +33,17 @@ const Footer = () => {
                         <p className="text-body lg:hidden">Hässelby Strands Sjöscouter</p>
                     </div>
                 </div>
-                <div className='lg:flex lg:flex-col gap-5 hidden lg:flex-1'>
+                <div className='lg:flex lg:flex-col gap-5 hidden lg:flex-1 '>
                     <p className="text-accent text-body-bold mb-2 uppercase text-xs lg:text-sm">{messages?.footer?.footer_service}</p>
-                    <div className='flex gap-2.5 leading-none '>
-                        <Image src={schoolIcon} alt="" className='w-4 h-4 xl:w-5 xl:h-7' />
+                    <div className='flex gap-2.5 leading-none items-center'>
+                        <Image src={schoolIcon} alt="" className='w-4 h-4 scale-[1.5] xl:scale-[1] xl:w-5 xl:h-7 items-center shrink-0 object-contain' />
                         <Link href="https://www.scouternasfolkhogskola.se/" target='_blank' className="hover:text-lightYellow text-body-desktop lg:text-lg" aria-label="Gå till Scouternas Folkhögskola">{messages?.footer?.footer_school}</Link>
                     </div>
-                    <div className='flex gap-2.5 leading-none'>
+                    <div className='flex gap-2.5 leading-none items-center'>
                         <Image src={bagIcon} alt="" className='w-4 h-4 xl:w-5 xl:h-6' />
                         <Link href="https://www.scoutshop.se/" target='_blank' className="hover:text-lightYellow text-body-desktop lg:text-lg" aria-label="Gå till scoutshopen">{messages?.footer?.footer_shop}</Link>
                     </div>
-                    <div className='flex gap-2.5 leading-none '>
+                    <div className='flex gap-2.5 leading-none items-center'>
                         <Image src={keyIcon} alt="" className='w-4 h-4 xl:w-5.5 xl:h-5.5' />
                         <Link href="https://www.scoutnet.se/f/login" target='_blank' className="hover:text-lightYellow text-body-desktop lg:text-lg " aria-label="Logga in på scoutnet">Scoutnet</Link>
                     </div>
@@ -58,7 +58,7 @@ const Footer = () => {
                     </div>
                     <div className='hidden lg:flex gap-2.5 leading-[1.2]' >
                         <a href="https://www.google.com/maps/place/Ruffen+%7C+H%C3%A4sselby+Strands+Sj%C3%B6scoutk%C3%A5r/@59.3620719,17.7818116,13z/data=!4m6!3m5!1s0x465f9f000adaa135:0xf7c26dadb80cbb2f!8m2!3d59.3632201!4d17.8220044!16s%2Fg%2F11y8fdsts1?authuser=0&entry=ttu&g_ep=EgoyMDI2MDUyNi4wIKXMDSoASAFQAw%3D%3D" aria-label="Visa scoutstugan Ruffen i google maps" target='_blank' className='flex gap-2.5 leading-[1.2] hover:text-lightYellow'>
-                            <Image src={locationIcon} alt="" className='w-6 h-6' />
+                            <Image src={locationIcon} alt="" className='md:lg:w-4 lg:w-5.5 lg:h-5.5' />
                             <p className="text-body lg:text-lg">Hässelby strandväg<br />165 73 Hässelby</p>
                         </a>
                     </div>
@@ -67,13 +67,14 @@ const Footer = () => {
                     <p className="text-accent text-body-bold uppercase  text-xs lg:text-sm">{messages?.footer?.footer_followUs}</p>
                     <div className='flex gap-2.5 leading-none'>
                         <a href="https://www.instagram.com/hss_scout/" target='_blank' className='flex gap-2.5 leading-[1.2] hover:text-lightYellow' aria-label="Följ oss på Instagram">
-                            <Image src={instagramIcon} alt='instagram icon' className='w-4 h-auto xl:w-5' aria-hidden="true"/>
+                            <Image src={instagramIcon} alt='instagram icon' className='w-4 h-auto xl:w-5' aria-hidden="true" />
                             <p className="text-body lg:text-lg">@hss_scout</p>
                         </a>
                     </div>
                     <div className='flex gap-2.5 leading-none'>
                         <a href="https://www.facebook.com/HSSScout/" target='_blank' className='flex gap-2.5 leading-[1.2] hover:text-lightYellow' aria-label="Följ oss på Facebook">
-                            <Facebook color="#FDB715" className="w-4 h-auto xl:w-5" aria-hidden="true"/>
+
+                            <Facebook color="#FDB715" className="w-4 h-auto xl:w-5 scale-[1.25]" aria-hidden="true" />
                             <p className="text-body lg:text-lg">@HSSScout</p>
                         </a>
                     </div>
